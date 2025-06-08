08db13e14babe3eb9e652fb810a0859510e92f57
3illBaby <thrillanon@outlook.com>
2025-06-08 08:34:33 +0100

    feat(app.module.ts): registered configuration modules and jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import config from '@/shared/config/config';
+import { ConfigifyModule } from '@itgorillaz/configify';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigifyModule.forRootAsync(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      load: [config],
+    }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get('jwt.secret'),
+        signOptions: {
+          expiresIn: config.get('jwt.expiresIn'),
+        },
+      }),
+      global: true,
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
