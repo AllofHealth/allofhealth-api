000a66bad09289c33d824bf3a990701314aea0f5
3illBaby <thrillanon@outlook.com>
2025-06-08 08:22:39 +0100

    chore(main.ts): configured main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { PORT } from './shared/data/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('AllofHealth API')
+    .setDescription('The AllofHealth API description')
+    .setVersion('1.0')
+    .addTag('health')
+    .addApiKey(
+      {
+        type: 'apiKey',
+        name: 'Authorization',
+        in: 'header',
+      },
+      'Authorization',
+    )
+    .build();
+
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api');
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(PORT);
 }
-bootstrap();
+bootstrap().catch((error) => {
+  console.error(error);
+});
