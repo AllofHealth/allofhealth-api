bf6901c3af5eae84dbd62b9036899a99134f0ae7
3illBaby <thrillanon@outlook.com>
2025-06-08 16:48:31 +0100

    feat(external-account.service.ts): consumed provider to create wallet

--- src/shared/modules/external-account/service/external-account.service.ts
+++ src/shared/modules/external-account/service/external-account.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { ExternalAccountProvider } from '../provider/external-account.provider';
 
 @Injectable()
-export class ExternalAccountService {}
+export class ExternalAccountService {
+  constructor(
+    private readonly externalAccountProvider: ExternalAccountProvider,
+  ) {}
+
+  createExternalWallet() {
+    return this.externalAccountProvider.createWallet();
+  }
+}
