14f7f7704e33859ce99769123379d5e3b683222d
3illBaby <thrillanon@outlook.com>
2025-06-08 13:55:29 +0100

    chore(drizzle.provider.ts): configured drizzle provider

---
+++ src/shared/drizzle/drizzle.provider.ts
@@ -0,0 +1,38 @@
+import { drizzle } from 'drizzle-orm/neon-serverless';
+import { Provider } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Pool } from '@neondatabase/serverless';
+import * as schema from '@/schemas/schema';
+
+export const DRIZZLE_PROVIDER = 'DRIZZLE_PROVIDER';
+export const drizzleProvider: Provider[] = [
+  {
+    provide: DRIZZLE_PROVIDER,
+    inject: [ConfigService],
+    useFactory: (configService: ConfigService) => {
+      const uri = configService.get<string>('database.uri');
+      if (!uri) {
+        throw new Error('Database URI is not defined, check config');
+      }
+
+      const pool = new Pool({
+        connectionString: uri,
+        max: 10,
+        connectionTimeoutMillis: 50000,
+      });
+
+      console.log('Attempting to connect to db');
+
+      pool
+        .connect()
+        .then((client) => {
+          client.release();
+          console.log('Successfully connected to db');
+        })
+        .catch((error) => {
+          console.error('Failed to connect to db', error);
+        });
+      return drizzle(pool, { schema });
+    },
+  },
+];
