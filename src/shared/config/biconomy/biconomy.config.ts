611a9f844217d812ae02c555aba494d742512776
3illBaby <thrillanon@outlook.com>
2025-06-08 15:04:21 +0100

    chore(biconomy.config.ts): created config

---
+++ src/shared/config/biconomy/biconomy.config.ts
@@ -0,0 +1,31 @@
+import { IsNotEmpty } from 'class-validator';
+import { Configuration, Value } from '@itgorillaz/configify';
+
+@Configuration()
+export class BiconomyConfig {
+  // Testnet Config
+  @IsNotEmpty()
+  @Value('LISK_TESTNET_BUNDLER_API_KEY')
+  LISK_TESTNET_BUNDLER_API_KEY: string;
+
+  @IsNotEmpty()
+  @Value('LISK_TESTNET_BUNDLER_URL')
+  LISK_TESTNET_BUNDLER_URL: string;
+
+  @IsNotEmpty()
+  @Value('LISK_TESTNET_BUNDLER_ID')
+  LISK_TESTNET_BUNDLER_ID: string;
+
+  // Mainnet config
+  @IsNotEmpty()
+  @Value('LISK_MAINNET_BUNDLER_API_KEY')
+  LISK_MAINNET_BUNDLER_API_KEY: string;
+
+  @IsNotEmpty()
+  @Value('LISK_MAINNET_BUNDLER_URL')
+  LISK_MAINNET_BUNDLER_URL: string;
+
+  @IsNotEmpty()
+  @Value('LISK_MAINNET_BUNDLER_ID')
+  LISK_MAINNET_BUNDLER_ID: string;
+}
