7c1568e128fd03cf0ff70756dc0f6ef87c0fd984
3illBaby <thrillanon@outlook.com>
2025-06-08 16:18:43 +0100

    chore(constants.ts): added lisk cofiguration

--- src/shared/data/constants.ts
+++ src/shared/data/constants.ts
@@ -1,2 +1,10 @@
 export const EXPIRES_IN = '7d';
 export const PORT = 3000;
+
+// Lisk mainnet
+export const LISK_MAINNET_RPC_URL = 'https://rpc.api.lisk.com';
+export const LISK_MAINNET_CHAINID = 1135;
+
+// Lisk testnet
+export const LISK_TESTNET_RPC_URL = 'https://rpc.sepolia-api.lisk.com';
+export const LISK_TESTNET_CHAINID = 4202;
