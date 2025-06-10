6aade166f12c2ef3077aec702e076b1497e79833
3illBaby <thrillanon@outlook.com>
2025-06-10 09:50:02 +0100

    test(external-accoubt.service.spec.ts): added test case to create wallet

--- src/shared/modules/external-account/service/external-account.service.spec.ts
+++ src/shared/modules/external-account/service/external-account.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ExternalAccountService } from './external-account.service';
+import { ExternalAccountProvider } from '../provider/external-account.provider';
 
 describe('ExternalAccountService', () => {
   let service: ExternalAccountService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ExternalAccountService],
+      providers: [ExternalAccountService, ExternalAccountProvider],
     }).compile();
 
     service = module.get<ExternalAccountService>(ExternalAccountService);
@@ -14,5 +15,14 @@ describe('ExternalAccountService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('Create external Wallet', () => {
+    it('should create a wallet', () => {
+      const result = service.createExternalWallet();
+
+      console.log(result);
+      expect(result).toBeDefined();
+    });
   });
 });
