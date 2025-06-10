4fefe1f8147f8ee4748f7c768bb52bd862ab564c
3illBaby <thrillanon@outlook.com>
2025-06-10 10:03:31 +0100

    refactor(external-account.service.ts): consumed error handler

--- src/shared/modules/external-account/service/external-account.service.ts
+++ src/shared/modules/external-account/service/external-account.service.ts
@@ -4,24 +4,29 @@ import {
   ExternalAccountErrorMessage,
   ExternalAccountSuccessMessage,
 } from '../data/external-account.data';
+import { ErrorHandler } from '@/shared/error-handler/error.handler';
 
 @Injectable()
 export class ExternalAccountService {
+  private errorHandler: ErrorHandler;
+
   constructor(
     private readonly externalAccountProvider: ExternalAccountProvider,
-  ) {}
+  ) {
+    this.errorHandler = new ErrorHandler();
+  }
 
   createExternalWallet() {
     const result = this.externalAccountProvider.handleCreateWallet();
     if (result.isErr()) {
-      return this.externalAccountProvider.handleError(
+      return this.errorHandler.handleError(
         result.error,
         ExternalAccountErrorMessage.FAILED_TO_CREATE_WALLET,
       );
     }
 
     if (result.isOk()) {
-      return this.externalAccountProvider.handleResult(
+      return this.errorHandler.handleResult(
         result,
         ExternalAccountSuccessMessage.WALLET_CREATED,
       );
