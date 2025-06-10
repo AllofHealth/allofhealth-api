da1db5803715a3e25dfa80e3ee34c687b3bfec33
3illBaby <thrillanon@outlook.com>
2025-06-10 15:28:20 +0100

    feat(account-abstraction.service.ts): impl func to create smart waller
    consumed provider func

--- src/shared/modules/account-abstraction/service/account-abstraction.service.ts
+++ src/shared/modules/account-abstraction/service/account-abstraction.service.ts
@@ -1,4 +1,30 @@
+import { ErrorHandler } from '@/shared/error-handler/error.handler';
 import { Injectable } from '@nestjs/common';
+import {
+  AccountAbstractionErrorMessage,
+  AccountAbstractionSuccessMessage,
+} from '../data/account-abstraction.data';
+import { AccountAbstractionProvider } from '../provider/account-abstraction.provider';
 
 @Injectable()
-export class AccountAbstractionService {}
+export class AccountAbstractionService {
+  private errorHandler: ErrorHandler;
+  constructor(private readonly provider: AccountAbstractionProvider) {
+    this.errorHandler = new ErrorHandler();
+  }
+
+  async createSmartAccount() {
+    const result = await this.provider.createSmartAccount();
+    if (result.isErr()) {
+      return this.errorHandler.handleError(
+        result.error,
+        AccountAbstractionErrorMessage.ERROR_CREATING_SMART_ACCOUNT,
+      );
+    }
+
+    return this.errorHandler.handleResult(
+      result,
+      AccountAbstractionSuccessMessage.SMART_ACCOUNT_CREATED,
+    );
+  }
+}
