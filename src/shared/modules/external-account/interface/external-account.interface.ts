a86e4c2e6e512b5231b25190590c5262454685be
3illBaby <thrillanon@outlook.com>
2025-06-10 09:45:20 +0100

    chore(external-acccount.interface.ts): created result types

--- src/shared/modules/external-account/interface/external-account.interface.ts
+++ src/shared/modules/external-account/interface/external-account.interface.ts
@@ -1,1 +1,15 @@
-export interface ExternalAccount {}
+import { Result } from 'neverthrow';
+import {
+  ProviderError,
+  WalletCreationError,
+} from '../errors/external-account.errors';
+import { ethers } from 'ethers';
+
+type WalletData = {
+  publicKey: string;
+  privateKey: string;
+  walletAddress: string;
+};
+
+export type TWalletResult = Result<WalletData, WalletCreationError>;
+export type TProviderResult = Result<ethers.JsonRpcProvider, ProviderError>;
