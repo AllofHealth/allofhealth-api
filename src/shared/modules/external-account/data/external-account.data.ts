fb229532815e28d897e33ec2a3f26c38c75a873a
3illBaby <thrillanon@outlook.com>
2025-06-10 09:44:24 +0100

    chore(external-account.data.ts): added data

--- src/shared/modules/external-account/data/external-account.data.ts
+++ src/shared/modules/external-account/data/external-account.data.ts
@@ -1,7 +1,10 @@
 export enum ExternalAccountSuccessMessage {
   WALLET_CREATED = 'Wallet created successfully',
+  PROVIDER_CONNECTED = 'Provider connected successfully',
 }
 
 export enum ExternalAccountErrorMessage {
   FAILED_TO_CREATE_WALLET = 'Failed to create wallet',
+  INVALID_RPC_URL = 'Invalid RPC URL',
+  PROVIDER_CONNECTION_FAILED = 'Failed to connect to provider',
 }
