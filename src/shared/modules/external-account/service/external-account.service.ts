102e47260f552bdc8d550696b907116c7bd85ffe
3illBaby <thrillanon@outlook.com>
2025-06-10 13:28:02 +0100

    chore(external-account.service.ts): consumed provider

--- src/shared/modules/external-account/service/external-account.service.ts
+++ src/shared/modules/external-account/service/external-account.service.ts
@@ -1,52 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { ExternalAccountProvider } from '../provider/external-account.provider';
-import {
-  ExternalAccountErrorMessage,
-  ExternalAccountSuccessMessage,
-} from '../data/external-account.data';
-import { ErrorHandler } from '@/shared/error-handler/error.handler';
 
 @Injectable()
 export class ExternalAccountService {
-  private errorHandler: ErrorHandler;
-
   constructor(
     private readonly externalAccountProvider: ExternalAccountProvider,
-  ) {
-    this.errorHandler = new ErrorHandler();
-  }
+  ) {}
 
   createExternalWallet() {
-    const result = this.externalAccountProvider.handleCreateWallet();
-    if (result.isErr()) {
-      return this.errorHandler.handleError(
-        result.error,
-        ExternalAccountErrorMessage.FAILED_TO_CREATE_WALLET,
-      );
-    }
-
-    if (result.isOk()) {
-      return this.errorHandler.handleResult(
-        result,
-        ExternalAccountSuccessMessage.WALLET_CREATED,
-      );
-    }
+    return this.externalAccountProvider.handleCreateWallet();
   }
 
   createNewSigner() {
-    const result = this.externalAccountProvider.createSigner();
-    if (result.isErr()) {
-      return this.errorHandler.handleError(
-        result.error,
-        ExternalAccountErrorMessage.ERROR_CREATING_SIGNER,
-      );
-    }
-
-    if (result.isOk()) {
-      return this.errorHandler.handleResult(
-        result,
-        ExternalAccountSuccessMessage.SIGNER_CREATED,
-      );
-    }
+    return this.externalAccountProvider.createSigner();
   }
 }
