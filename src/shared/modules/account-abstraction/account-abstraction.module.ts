041d224782fa21c2b18d43252927f7f8da5bad63
3illBaby <thrillanon@outlook.com>
2025-06-10 14:00:09 +0100

    chore(account-abstraction.module.ts): imported eoa

--- src/shared/modules/account-abstraction/account-abstraction.module.ts
+++ src/shared/modules/account-abstraction/account-abstraction.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AccountAbstractionService } from './service/account-abstraction.service';
 import { AccountAbstractionProvider } from './provider/account-abstraction.provider';
+import { ExternalAccountModule } from '../external-account/external-account.module';
 
 @Module({
+  imports: [ExternalAccountModule],
   providers: [AccountAbstractionService, AccountAbstractionProvider],
   exports: [AccountAbstractionService],
 })
