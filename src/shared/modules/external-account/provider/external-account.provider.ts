952e6848dad934793c1f395827a1606f9886eca1
3illBaby <thrillanon@outlook.com>
2025-06-10 10:03:02 +0100

    refactor(external-account.provider.ts): yanked error handlers from file
    moved to a global file for reuseability

--- src/shared/modules/external-account/provider/external-account.provider.ts
+++ src/shared/modules/external-account/provider/external-account.provider.ts
@@ -2,76 +2,27 @@ import {
   LISK_MAINNET_RPC_URL,
   LISK_TESTNET_RPC_URL,
 } from '@/shared/data/constants';
+import { Injectable } from '@nestjs/common';
 import { ethers } from 'ethers';
-import { HttpStatus, Injectable } from '@nestjs/common';
-import { MyLoggerService } from '@/modules/my-logger/service/my-logger.service';
-import { IHandleReturn } from '@/shared/interface/shared.interface';
+import { err, fromThrowable, ok } from 'neverthrow';
 import { ExternalAccountErrorMessage } from '../data/external-account.data';
-import { Result, ok, err, fromThrowable } from 'neverthrow';
-import {
-  TProviderResult,
-  TWalletResult,
-} from '../interface/external-account.interface';
 import {
   ProviderError,
   WalletCreationError,
 } from '../errors/external-account.errors';
+import {
+  TProviderResult,
+  TWalletResult,
+} from '../interface/external-account.interface';
 
 @Injectable()
 export class ExternalAccountProvider {
-  private readonly logger = new MyLoggerService(ExternalAccountProvider.name);
   private readonly rpcUrl: string;
   constructor() {
     this.rpcUrl =
       process.env.NODE_ENV === 'production'
         ? LISK_MAINNET_RPC_URL
         : LISK_TESTNET_RPC_URL;
-  }
-
-  handleResult<T, E extends Error>(
-    result: Result<T, E>,
-    successMessage: string,
-  ) {
-    return result.match(
-      (data) =>
-        this.handleReturn({
-          status: HttpStatus.OK,
-          message: successMessage,
-          data,
-        }),
-      (error) => this.handleError(error, error.message),
-    );
-  }
-
-  handleError(error: any, context: string) {
-    console.error(error, context);
-    this.logger.error(`${context}: ${error.message}`);
-    if (error.status) {
-      return {
-        //eslint-disable-next-line
-        status: error.status,
-        //eslint-disable-next-line
-        message: error.message || context,
-      };
-    }
-
-    return {
-      status: HttpStatus.INTERNAL_SERVER_ERROR,
-      message: context,
-      //eslint-disable-next-line
-      error: process.env.NODE_ENV !== 'production' ? error.message : undefined,
-    };
-  }
-
-  private handleReturn<T, D = undefined, M = undefined>(
-    args: IHandleReturn<T, D, M>,
-  ) {
-    return {
-      status: args.status,
-      message: args.message,
-      data: args.data ? args.data : null,
-      meta: args.meta ? args.meta : null,
-    };
   }
 
   handleGetProvider(): TProviderResult {
