6f4a57d5ce5835446fa2753c3e57ff3c97908c5d
3illBaby <thrillanon@outlook.com>
2025-06-10 09:46:08 +0100

    refactor(external-account.provider): refactored to use neverThrow
    ensures granular error handling

--- src/shared/modules/external-account/provider/external-account.provider.spec.ts
+++ src/shared/modules/external-account/provider/external-account.provider.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ExternalAccountProvider } from './external-account.provider';
+import { ExternalAccountSuccessMessage } from '../data/external-account.data';
 
 describe('ExternalAccountProvider', () => {
   let provider: ExternalAccountProvider;
@@ -18,11 +19,23 @@ describe('ExternalAccountProvider', () => {
 
   describe('Wallet creation', () => {
     it('should create an ethereum wallet', () => {
-      const wallet = provider.createWallet();
-      console.log(wallet);
-      expect(wallet).toBeDefined();
-      expect(wallet.data?.walletAddress).toBeDefined();
-      expect(wallet.data?.privateKey).toBeDefined();
+      const result = provider.handleCreateWallet();
+
+      expect(result.isOk()).toBe(true);
+
+      const wallet = provider.handleResult(
+        result,
+        ExternalAccountSuccessMessage.WALLET_CREATED,
+      );
+
+      if (!('data' in wallet) || !(wallet.data && wallet)) {
+        throw new Error('Wallet data not found');
+      }
+
+      expect(wallet.data).toBeDefined();
+      expect(wallet.data.publicKey).toBeDefined();
+      expect(wallet.data.privateKey).toBeDefined();
+      expect(wallet.data.walletAddress).toBeDefined();
     });
   });
 });
