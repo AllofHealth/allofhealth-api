f3a935b37c368d0193b106273225dea3fc728b6c
3illBaby <thrillanon@outlook.com>
2025-06-10 10:01:55 +0100

    chore(error.handler.ts): global class to handle error and handle result

---
+++ src/shared/error-handler/error.handler.ts
@@ -0,0 +1,54 @@
+import { MyLoggerService } from '@/modules/my-logger/service/my-logger.service';
+import { HttpStatus } from '@nestjs/common';
+import { Result } from 'neverthrow';
+import { IHandleReturn } from '../interface/shared.interface';
+
+export class ErrorHandler {
+  private readonly logger = new MyLoggerService(ErrorHandler.name);
+
+  handleResult<T, E extends Error>(
+    result: Result<T, E>,
+    successMessage: string,
+  ) {
+    return result.match(
+      (data) =>
+        this.handleReturn({
+          status: HttpStatus.OK,
+          message: successMessage,
+          data,
+        }),
+      (error) => this.handleError(error, error.message),
+    );
+  }
+
+  handleError(error: any, context: string) {
+    console.error(error, context);
+    this.logger.error(`${context}: ${error.message}`);
+    if (error.status) {
+      return {
+        //eslint-disable-next-line
+        status: error.status,
+        //eslint-disable-next-line
+        message: error.message || context,
+      };
+    }
+
+    return {
+      status: HttpStatus.INTERNAL_SERVER_ERROR,
+      message: context,
+      //eslint-disable-next-line
+      error: process.env.NODE_ENV !== 'production' ? error.message : undefined,
+    };
+  }
+
+  private handleReturn<T, D = undefined, M = undefined>(
+    args: IHandleReturn<T, D, M>,
+  ) {
+    return {
+      status: args.status,
+      message: args.message,
+      data: args.data ? args.data : null,
+      meta: args.meta ? args.meta : null,
+    };
+  }
+}
