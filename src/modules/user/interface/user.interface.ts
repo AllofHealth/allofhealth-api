bff42e88887769594f1d67e941560d97c0202710
3illBaby <thrillanon@outlook.com>
2025-06-12 18:41:40 +0100

    chore(user.interface.ts): added interface

--- src/modules/user/interface/user.interface.ts
+++ src/modules/user/interface/user.interface.ts
@@ -1,1 +1,30 @@
-export interface User {}
+import { TRole } from '@/shared/interface/shared.interface';
+
+export interface ICreateDoctor {
+  specialization: string;
+  medicalLicenseNumber: string;
+  scannedLicenseUrl: string;
+  yearsOfExperience: number;
+  certifications: string[];
+  hospitalAssociation: string;
+  locationOfHospital: string;
+  languagesSpoken: string[];
+}
+
+export interface ICreateUser {
+  fullName: string;
+  emailAddress: string;
+  dateOfBirth: Date;
+  gender: string;
+  phoneNumber: string;
+  governmentIdUrl: string;
+  specialization?: string;
+  medicalLicenseNumber?: string;
+  scannedLicenseUrl?: string;
+  yearsOfExperience?: number;
+  certifications?: string[];
+  hospitalAssociation?: string;
+  locationOfHospital?: string;
+  languagesSpoken?: string[];
+  role: TRole;
+}
