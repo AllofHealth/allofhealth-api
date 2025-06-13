59e9e618fd8070a594688d2d897f6985447ab591
3illBaby <thrillanon@outlook.com>
2025-06-14 00:40:41 +0100

    Add AccountQueueModule as import to UserModule

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,8 +3,10 @@ import { UserProvider } from './provider/user.provider';
 import { UserService } from './service/user.service';
 import { UserController } from './controller/user.controller';
 import { AuthUtils } from '@/shared/utils/auth.utils';
+import { AccountQueueModule } from '@/shared/queues/account/account-queue.module';
 
 @Module({
+  imports: [AccountQueueModule],
   providers: [UserProvider, UserService, AuthUtils],
   controllers: [UserController],
   exports: [UserService],
