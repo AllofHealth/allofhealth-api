7a1f2a55ab9397cc3071a42fa7ebc4d081609e87
3illBaby <thrillanon@outlook.com>
2025-06-13 10:05:41 +0100

    chore(shared.events.dto): added event dto

---
+++ src/shared/dtos/event.dto.ts
@@ -0,0 +1,14 @@
+export class CreateDoctor {
+  constructor(
+    readonly userId: string,
+    readonly specialization: string,
+    readonly medicalLicenseNumber: string,
+    readonly scannedLicenseUrl: string,
+    readonly yearsOfExperience: number,
+    readonly certifications: string[],
+    readonly hospitalAssociation: string,
+    readonly locationOfHospital: string,
+    readonly languagesSpoken: string[],
+    readonly licenseExpirationDate: Date,
+  ) {}
+}
