de5ba6a8cc179725e3c528521bc619ae0d6c47bb
3illBaby <thrillanon@outlook.com>
2025-06-13 20:25:27 +0100

    chore(identity.data.ts): added error and success messages

---
+++ src/modules/identity/data/identity.data.ts
@@ -0,0 +1,7 @@
+export enum IDENTTIY_ERROR_MESSAGES {
+  ERROR_STORING_IDENTIFICATION = 'Error storing identification',
+}
+
+export enum IDENTITY_SUCCESS_MESSAGES {
+  SUCCESS_STORING_IDENTIFICATION = 'Identification stored successfully',
+}
