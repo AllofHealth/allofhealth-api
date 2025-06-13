778cf2b7858b21e6623e7de162deeb2e61bc5ce0
3illBaby <thrillanon@outlook.com>
2025-06-13 21:27:54 +0100

    feat(identity.provider.ts): impl func to store identification docs

--- src/modules/identity/provider/identity.provider.ts
+++ src/modules/identity/provider/identity.provider.ts
@@ -1,4 +1,72 @@
-import { Injectable } from '@nestjs/common';
+import * as schema from '@/schemas/schema';
+import { DRIZZLE_PROVIDER } from '@/shared/drizzle/drizzle.provider';
+import { Database } from '@/shared/drizzle/drizzle.types';
+import { Inject, Injectable } from '@nestjs/common';
+import { eq } from 'drizzle-orm';
+import { err, ResultAsync } from 'neverthrow';
+import { IdentityError } from '../error/identity.error';
+import { IStoreIdentification } from '../interface/identity.interface';
 
 @Injectable()
-export class IdentityProvider {}
+export class IdentityProvider {
+  constructor(@Inject(DRIZZLE_PROVIDER) private readonly db: Database) {}
+
+  private fetchUser(userId: string) {
+    return ResultAsync.fromPromise(
+      this.db
+        .select()
+        .from(schema.user)
+        .where(eq(schema.user.id, userId))
+        .limit(1),
+      (error: Error) => new IdentityError(error.message),
+    );
+  }
+
+  storeId(ctx: IStoreIdentification) {
+    return this.fetchUser(ctx.userId).andThen((user) => {
+      if (user.length === 0) {
+        return err(new IdentityError(`user not found`));
+      }
+
+      switch (ctx.role) {
+        case 'DOCTOR':
+          if (!ctx.scannedLicense) {
+            return err(new IdentityError('scanned license is required'));
+          }
+
+          return ResultAsync.fromPromise(
+            this.db
+              .insert(schema.identity)
+              .values({
+                userId: ctx.userId,
+                governmentId: ctx.governmentId,
+                scannedLicense: ctx.scannedLicense,
+                role: 'DOCTOR',
+              })
+              .returning(),
+            (error: Error) => new IdentityError(error.message),
+          ).map((identity) => ({
+            id: identity[0].id,
+          }));
+
+        case 'PATIENT':
+          return ResultAsync.fromPromise(
+            this.db
+              .insert(schema.identity)
+              .values({
+                userId: ctx.userId,
+                governmentId: ctx.governmentId,
+                role: 'PATIENT',
+              })
+              .returning(),
+            (error: Error) => new IdentityError(error.message),
+          ).map((identity) => ({
+            id: identity[0].id,
+          }));
+
+        default:
+          return err(new IdentityError(`invalid role`));
+      }
+    });
+  }
+}
