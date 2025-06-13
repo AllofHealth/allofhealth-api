ff746892e9ceaca334aa4ad77f15d12c9a029d07
3illBaby <thrillanon@outlook.com>
2025-06-13 16:41:49 +0100

    chore(user.data.ts): added user data

--- src/modules/user/data/user.data.ts
+++ src/modules/user/data/user.data.ts
@@ -2,4 +2,5 @@ export enum USER_ERROR_MESSAGES {
   ERROR_CREATE_USER = 'Error creating user',
   EMAIL_EXIST = 'Email already exist please log in',
   USER_NOT_FOUND = 'User not found',
+  USER_EXISTS = 'User already exists',
 }
