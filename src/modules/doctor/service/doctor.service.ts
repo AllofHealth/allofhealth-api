218b3ae2d3f55e23c713d9826b41ccb803bbbd1e
3illBaby <thrillanon@outlook.com>
2025-06-13 09:53:31 +0100

    feat(doctor.service.ts): impl function to create doctor

--- src/modules/doctor/service/doctor.service.ts
+++ src/modules/doctor/service/doctor.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { DoctorProvider } from '../provider/doctor.provider';
+import { ErrorHandler } from '@/shared/error-handler/error.handler';
+import { ICreateDoctor } from '../interface/doctor.interface';
+import { DOCTOR_SUCCESS_MESSAGES } from '../data/doctor.data';
 
 @Injectable()
-export class DoctorService {}
+export class DoctorService {
+  private errorHandler: ErrorHandler;
+  constructor(private readonly doctorProvider: DoctorProvider) {
+    this.errorHandler = new ErrorHandler();
+  }
+
+  async createDoctor(ctx: ICreateDoctor) {
+    const result = this.doctorProvider.createDoctor(ctx);
+    return await this.errorHandler.handleResult(
+      result,
+      DOCTOR_SUCCESS_MESSAGES.DOCTOR_CREATED,
+    );
+  }
+}
