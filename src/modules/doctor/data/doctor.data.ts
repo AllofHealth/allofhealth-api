a325ac0826f5215818132969c12a3fcabff9d572
3illBaby <thrillanon@outlook.com>
2025-06-13 09:32:11 +0100

    chore(doctor.data.ts): added doctor datya

--- src/modules/doctor/data/doctor.data.ts
+++ src/modules/doctor/data/doctor.data.ts
@@ -1,3 +1,5 @@
 export enum DOCTOR_ERROR_MESSGAES {
   ERROR_CREATING_DOCTOR = 'Error creating doctor',
+  DOCTOR_NOT_FOUND = 'Doctor not found',
+  DOCTOR_ALREADY_EXISTS = 'Doctor already exists',
 }
