196eb687a9935f52052805e26a5c8ad0d6f3b80f
3illBaby <thrillanon@outlook.com>
2025-06-13 07:47:33 +0100

    chore(doctor.interface.ts): added interface

--- src/modules/doctor/interface/doctor.interface.ts
+++ src/modules/doctor/interface/doctor.interface.ts
@@ -1,1 +1,10 @@
-export interface Doctor {}
+export interface ICreateDoctor {
+  specialization: string;
+  medicalLicenseNumber: string;
+  scannedLicenseUrl: string;
+  yearsOfExperience: number;
+  certifications: string[];
+  hospitalAssociation: string;
+  locationOfHospital: string;
+  languagesSpoken: string[];
+}
