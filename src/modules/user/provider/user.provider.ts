d5fb872274ca1e2d7877f8d72bde06c8e750d42d
3illBaby <thrillanon@outlook.com>
2025-06-13 16:43:06 +0100

    feat(user.provider.ts): impl function to create user and handle role document created

--- src/modules/user/provider/user.provider.ts
+++ src/modules/user/provider/user.provider.ts
@@ -6,13 +6,23 @@ import { eq } from 'drizzle-orm';
 import { err, ok, ResultAsync } from 'neverthrow';
 import { USER_ERROR_MESSAGES } from '../data/user.data';
 import { UserError } from '../error/user.error';
+import { EventEmitter2, OnEvent } from '@nestjs/event-emitter';
+import { ICreateUser } from '../interface/user.interface';
+import { AuthUtils } from '@/shared/utils/auth.utils';
+import { SharedEvents } from '@/shared/events/shared.events';
+import { CreateDoctor, DeleteUser } from '@/shared/dtos/event.dto';
+import { ICreateDoctor } from '@/modules/doctor/interface/doctor.interface';
 
 @Injectable()
 export class UserProvider {
-  constructor(@Inject(DRIZZLE_PROVIDER) private readonly db: Database) {}
+  constructor(
+    @Inject(DRIZZLE_PROVIDER) private readonly db: Database,
+    private readonly authUtils: AuthUtils,
+    private readonly eventEmitter: EventEmitter2,
+  ) {}
 
   validateEmailAddress(emailAddress: string) {
-    this.findUserByEmail(emailAddress)
+    return this.findUserByEmail(emailAddress)
       .map(() => true)
       .orElse((error) => {
         if (error.message.includes(USER_ERROR_MESSAGES.USER_NOT_FOUND)) {
@@ -52,6 +62,115 @@ export class UserProvider {
       }
 
       return ok(user[0]);
+    });
+  }
+
+  @OnEvent(SharedEvents.DELETE_USER)
+  deleteUser(ctx: DeleteUser) {
+    return ResultAsync.fromPromise(
+      this.db.delete(schema.user).where(eq(schema.user.id, ctx.userId)),
+      (error: Error) =>
+        new UserError(`Failed to delete user by id ${error.message}`),
+    ).andThen(() => {
+      return ok(true);
+    });
+  }
+
+  private async emitEvent(ctx: ICreateDoctor) {
+    try {
+      await this.eventEmitter.emitAsync(
+        SharedEvents.CREATE_DOCTOR,
+        new CreateDoctor(
+          ctx.userId,
+          ctx.specialization,
+          ctx.medicalLicenseNumber,
+          ctx.scannedLicenseUrl,
+          ctx.yearsOfExperience,
+          ctx.certifications,
+          ctx.hospitalAssociation,
+          ctx.locationOfHospital,
+          ctx.languagesSpoken,
+          ctx.licenseExpirationDate,
+        ),
+      );
+
+      return ok(true);
+    } catch (error) {
+      //handle rollback
+      this.eventEmitter.emit(
+        SharedEvents.DELETE_USER,
+        new DeleteUser(ctx.userId),
+      );
+      return err(new Error(`Failed to emit event ${error}`));
+    }
+  }
+
+  async createUser(ctx: ICreateUser) {
+    const hashedPassword = await this.authUtils.hash(ctx.password);
+    return this.validateEmailAddress(ctx.emailAddress).andThen((userExists) => {
+      if (userExists) {
+        return err(new UserError(USER_ERROR_MESSAGES.USER_EXISTS));
+      }
+
+      const dateOfBirthString = ctx.dateOfBirth.toISOString().split('T')[0];
+
+      return ResultAsync.fromPromise(
+        this.db
+          .insert(schema.user)
+          .values({
+            fullName: ctx.fullName,
+            emailAddress: ctx.emailAddress,
+            dateOfBirth: dateOfBirthString,
+            gender: ctx.gender,
+            password: hashedPassword,
+            phoneNumber: ctx.phoneNumber,
+            role: ctx.role,
+          })
+          .returning(),
+        (error: Error) =>
+          new UserError(
+            `${USER_ERROR_MESSAGES.ERROR_CREATE_USER} ${error.message}`,
+          ),
+      ).andThen((insertedUser) => {
+        if (!insertedUser[0]) {
+          return err(new UserError(USER_ERROR_MESSAGES.ERROR_CREATE_USER));
+        }
+
+        const user = insertedUser[0];
+
+        switch (ctx.role) {
+          case 'PATIENT':
+            return ok(user);
+          case 'DOCTOR':
+            return ResultAsync.fromPromise(
+              this.emitEvent({
+                userId: insertedUser[0].id,
+                certifications: ctx.certifications!,
+                hospitalAssociation: ctx.hospitalAssociation!,
+                languagesSpoken: ctx.languagesSpoken!,
+                licenseExpirationDate: ctx.licenseExpirationDate!,
+                locationOfHospital: ctx.locationOfHospital!,
+                medicalLicenseNumber: ctx.medicalLicenseNumber!,
+                scannedLicenseUrl: ctx.scannedLicenseUrl!,
+                specialization: ctx.specialization!,
+                yearsOfExperience: ctx.yearsOfExperience!,
+              }),
+              (error: Error) =>
+                new UserError(
+                  `${USER_ERROR_MESSAGES.ERROR_CREATE_USER} ${error.message}`,
+                ),
+            ).andThen((result) => {
+              if (result.isOk()) {
+                return ok(insertedUser[0]);
+              } else {
+                return err(new UserError(result.error.message));
+              }
+            });
+
+          default:
+            return err(new UserError('Handlers for other roles not available'));
+        }
+      });
     });
   }
 }
