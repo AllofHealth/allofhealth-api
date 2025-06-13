7e51dec58dfee4c3c9adbea42127ca0fdb24d8c1
3illBaby <thrillanon@outlook.com>
2025-06-13 16:43:28 +0100

    chore(user.module.ts): injected auth utils

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserProvider } from './provider/user.provider';
 import { UserService } from './service/user.service';
 import { UserController } from './controller/user.controller';
+import { AuthUtils } from '@/shared/utils/auth.utils';
 
 @Module({
-  providers: [UserProvider, UserService],
+  providers: [UserProvider, UserService, AuthUtils],
   controllers: [UserController],
   exports: [UserService],
 })
