444a7b3859a9dee1c2dee4065f0f12be32fa754c
3illBaby <thrillanon@outlook.com>
2025-06-14 00:08:12 +0100

    Add type safety to smart account processor event

--- src/shared/processors/account.processor.ts
+++ src/shared/processors/account.processor.ts
@@ -3,6 +3,7 @@ import { Process, Processor } from '@nestjs/bull';
 import { EventEmitter2 } from '@nestjs/event-emitter';
 import { Job } from 'bull';
 import { SharedEvents } from '../events/shared.events';
+import { CreateSmartAccount } from '../dtos/event.dto';
 
 @Processor('create-smart-account-queue')
 export class CreateSmartAccountProcessor {
@@ -12,10 +13,13 @@ export class CreateSmartAccountProcessor {
   constructor(private readonly eventEmiiter: EventEmitter2) {}
 
   @Process('create-smart-account')
-  async handleCreateSmartAccount(job: Job) {
+  async handleCreateSmartAccount(job: Job<CreateSmartAccount>) {
     try {
       this.logger.debug('Processing create smart account');
-      await this.eventEmiiter.emitAsync(SharedEvents.CREATE_SMART_ACCOUNT);
+      await this.eventEmiiter.emitAsync(
+        SharedEvents.CREATE_SMART_ACCOUNT,
+        new CreateSmartAccount(job.data.userId),
+      );
     } catch (e) {
       this.logger.error(`Error creating smart account: ${job.id}`, e);
     }
