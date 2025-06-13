ea14f41297afd121cfee8197ad3beb86fada181d
3illBaby <thrillanon@outlook.com>
2025-06-14 00:05:56 +0100

    Add event context param to smart account creation

--- src/shared/modules/account-abstraction/service/account-abstraction.service.ts
+++ src/shared/modules/account-abstraction/service/account-abstraction.service.ts
@@ -1,12 +1,10 @@
+import { CreateSmartAccount } from '@/shared/dtos/event.dto';
 import { ErrorHandler } from '@/shared/error-handler/error.handler';
-import { Injectable } from '@nestjs/common';
-import {
-  AccountAbstractionErrorMessage,
-  AccountAbstractionSuccessMessage,
-} from '../data/account-abstraction.data';
-import { AccountAbstractionProvider } from '../provider/account-abstraction.provider';
-import { OnEvent } from '@nestjs/event-emitter';
 import { SharedEvents } from '@/shared/events/shared.events';
+import { Injectable } from '@nestjs/common';
+import { OnEvent } from '@nestjs/event-emitter';
+import { AccountAbstractionSuccessMessage } from '../data/account-abstraction.data';
+import { AccountAbstractionProvider } from '../provider/account-abstraction.provider';
 
 @Injectable()
 export class AccountAbstractionService {
@@ -16,14 +14,8 @@ export class AccountAbstractionService {
   }
 
   @OnEvent(SharedEvents.CREATE_SMART_ACCOUNT, { async: true })
-  async createSmartAccount() {
-    const result = await this.provider.createSmartAccount();
-    if (result.isErr()) {
-      return this.errorHandler.handleError(
-        result.error,
-        AccountAbstractionErrorMessage.ERROR_CREATING_SMART_ACCOUNT,
-      );
-    }
+  async createSmartAccount(ctx: CreateSmartAccount) {
+    const result = await this.provider.createSmartAccount(ctx.userId);
 
     return this.errorHandler.handleResult(
       result,
