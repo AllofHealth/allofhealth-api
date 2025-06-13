6247ee4fd9371d2986f829f35fcae86f65525747
3illBaby <thrillanon@outlook.com>
2025-06-13 22:31:48 +0100

    chore(config.ts): registered config

--- src/shared/config/config.ts
+++ src/shared/config/config.ts
@@ -11,4 +11,10 @@ export default () => ({
         ? process.env.DATABASE_URL
         : process.env.DATABASE_URL_STAGING,
   },
+  redis: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PASSWORD,
+    username: process.env.REDIS_USERNAME,
+  },
 });
