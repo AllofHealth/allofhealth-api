4f5080c8f539fa4b56647d52971a85c2956a59ed
3illBaby <thrillanon@outlook.com>
2025-06-13 20:08:46 +0100

    chore(user.service.ts): consumed provider

--- src/modules/user/service/user.service.ts
+++ src/modules/user/service/user.service.ts
@@ -1,4 +1,23 @@
+import { ErrorHandler } from '@/shared/error-handler/error.handler';
 import { Injectable } from '@nestjs/common';
+import { ResultAsync } from 'neverthrow';
+import { USER_SUCCESS_MESSAGE } from '../data/user.data';
+import { UserError } from '../error/user.error';
+import { ICreateUser } from '../interface/user.interface';
+import { UserProvider } from '../provider/user.provider';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private handler: ErrorHandler;
+  constructor(private readonly userProvider: UserProvider) {
+    this.handler = new ErrorHandler();
+  }
+
+  createUser(ctx: ICreateUser) {
+    const result = ResultAsync.fromPromise(
+      this.userProvider.createUser(ctx),
+      (error: Error) => new UserError(error.message),
+    );
+    return this.handler.handleResult(result, USER_SUCCESS_MESSAGE.USER_CREATED);
+  }
+}
