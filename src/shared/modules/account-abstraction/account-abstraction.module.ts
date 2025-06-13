2dca4501af6d37779930fb71dc63e79330f6ad33
3illBaby <thrillanon@outlook.com>
2025-06-14 00:25:13 +0100

    Add AuthUtils to AA module providers

--- src/shared/modules/account-abstraction/account-abstraction.module.ts
+++ src/shared/modules/account-abstraction/account-abstraction.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AccountAbstractionService } from './service/account-abstraction.service';
 import { AccountAbstractionProvider } from './provider/account-abstraction.provider';
 import { ExternalAccountModule } from '../external-account/external-account.module';
+import { AuthUtils } from '@/shared/utils/auth.utils';
 
 @Module({
   imports: [ExternalAccountModule],
-  providers: [AccountAbstractionService, AccountAbstractionProvider],
+  providers: [AccountAbstractionService, AccountAbstractionProvider, AuthUtils],
   exports: [AccountAbstractionService],
 })
 export class AccountAbstractionModule {}
