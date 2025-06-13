2370df652b6aa872f52e1c9920e6923f7df03e47
3illBaby <thrillanon@outlook.com>
2025-06-13 22:53:18 +0100

    Add CREATE_SMART_ACCOUNT shared event

--- src/shared/events/shared.events.ts
+++ src/shared/events/shared.events.ts
@@ -2,4 +2,5 @@ export enum SharedEvents {
   CREATE_DOCTOR = 'create.doctor',
   DELETE_USER = 'delete.user',
   STORE_IDENTIFICATION = 'store.identification',
+  CREATE_SMART_ACCOUNT = 'create.smart.account',
 }
