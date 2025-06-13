2e05f5239db38eab790644517c1239e1eb76e268
3illBaby <thrillanon@outlook.com>
2025-06-13 21:43:52 +0100

    Add identity storage functionality

--- src/modules/identity/service/identity.service.ts
+++ src/modules/identity/service/identity.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { IdentityProvider } from '../provider/identity.provider';
+import { ErrorHandler } from '@/shared/error-handler/error.handler';
+import { IStoreIdentification } from '../interface/identity.interface';
+import { IDENTITY_SUCCESS_MESSAGES } from '../data/identity.data';
 
 @Injectable()
-export class IdentityService {}
+export class IdentityService {
+  private handler: ErrorHandler;
+  constructor(private readonly identityProvider: IdentityProvider) {
+    this.handler = new ErrorHandler();
+  }
+
+  storeIdentity(ctx: IStoreIdentification) {
+    const result = this.identityProvider.storeId(ctx);
+    return this.handler.handleResult(
+      result,
+      IDENTITY_SUCCESS_MESSAGES.SUCCESS_STORING_IDENTIFICATION,
+    );
+  }
+}
