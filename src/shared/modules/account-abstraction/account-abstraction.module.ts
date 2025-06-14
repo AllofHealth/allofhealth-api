53dde09b6f0d8b7b8222fd84742ec806eb4f4441
3illBaby <thrillanon@outlook.com>
2025-06-14 23:08:43 +0100

    chore(account-abstraction.module.ts): registerd controller

--- src/shared/modules/account-abstraction/account-abstraction.module.ts
+++ src/shared/modules/account-abstraction/account-abstraction.module.ts
@@ -3,10 +3,12 @@ import { AccountAbstractionService } from './service/account-abstraction.service
 import { AccountAbstractionProvider } from './provider/account-abstraction.provider';
 import { ExternalAccountModule } from '../external-account/external-account.module';
 import { AuthUtils } from '@/shared/utils/auth.utils';
+import { AccountAbstractionController } from './controller/account-abstraction.controller';
 
 @Module({
   imports: [ExternalAccountModule],
   providers: [AccountAbstractionService, AccountAbstractionProvider, AuthUtils],
   exports: [AccountAbstractionService],
+  controllers: [AccountAbstractionController],
 })
 export class AccountAbstractionModule {}
