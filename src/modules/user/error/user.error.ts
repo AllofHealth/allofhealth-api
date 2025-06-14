a3fed64826a8e516d2ebed28357d2b5c4729425b
3illBaby <thrillanon@outlook.com>
2025-06-14 21:41:21 +0100

    Add HTTP status code to UserError class

--- src/modules/user/error/user.error.ts
+++ src/modules/user/error/user.error.ts
@@ -1,8 +1,15 @@
+import { HttpStatus } from '@nestjs/common';
 import { USER_ERROR_MESSAGES } from '../data/user.data';
 
 export class UserError extends Error {
-  constructor(message: string = USER_ERROR_MESSAGES.ERROR_CREATE_USER) {
+  public status: number;
+
+  constructor(
+    message: string = USER_ERROR_MESSAGES.ERROR_CREATE_USER,
+    status: number = HttpStatus.INTERNAL_SERVER_ERROR,
+  ) {
     super(message);
     this.name = 'UserError';
+    this.status = status;
   }
 }
