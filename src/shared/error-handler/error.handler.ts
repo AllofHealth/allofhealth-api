e2b20adba9c50514ebc9e020e43f69f6e9c8bc2a
3illBaby <thrillanon@outlook.com>
2025-06-14 21:41:45 +0100

    Remove handleResult method and make handleReturn public

--- src/shared/error-handler/error.handler.ts
+++ src/shared/error-handler/error.handler.ts
@@ -1,26 +1,9 @@
 import { MyLoggerService } from '@/modules/my-logger/service/my-logger.service';
 import { HttpStatus } from '@nestjs/common';
-import { Result, ResultAsync } from 'neverthrow';
 import { IHandleReturn } from '../interface/shared.interface';
 
 export class ErrorHandler {
   private readonly logger = new MyLoggerService(ErrorHandler.name);
-
-  handleResult<T, E extends Error>(
-    result: Result<T, E> | ResultAsync<T, E>,
-    successMessage: string,
-  ) {
-    return result.match(
-      (data) =>
-        this.handleReturn({
-          status: HttpStatus.OK,
-          message: successMessage,
-          data,
-        }),
-      (error) => this.handleError(error, error.message),
-    );
-  }
-
   handleError(error: any, context: string) {
     console.error(error, context);
     this.logger.error(`${context}: ${error.message}`);
@@ -41,9 +24,7 @@ export class ErrorHandler {
     };
   }
 
-  private handleReturn<T, D = undefined, M = undefined>(
-    args: IHandleReturn<T, D, M>,
-  ) {
+  handleReturn<T, D = undefined, M = undefined>(args: IHandleReturn<T, D, M>) {
     return {
       status: args.status,
       message: args.message,
