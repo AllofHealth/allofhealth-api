c1be9779592cea40d22307d2c6d7a019b8d0a630
3illBaby <thrillanon@outlook.com>
2025-06-14 21:40:16 +0100

    Remove error handling from doctor service

--- src/modules/doctor/service/doctor.service.ts
+++ src/modules/doctor/service/doctor.service.ts
@@ -1,24 +1,15 @@
 import { CreateDoctor } from '@/shared/dtos/event.dto';
-import { ErrorHandler } from '@/shared/error-handler/error.handler';
 import { SharedEvents } from '@/shared/events/shared.events';
 import { Injectable } from '@nestjs/common';
 import { OnEvent } from '@nestjs/event-emitter';
-import { DOCTOR_SUCCESS_MESSAGES } from '../data/doctor.data';
 import { DoctorProvider } from '../provider/doctor.provider';
 
 @Injectable()
 export class DoctorService {
-  private errorHandler: ErrorHandler;
-  constructor(private readonly doctorProvider: DoctorProvider) {
-    this.errorHandler = new ErrorHandler();
-  }
+  constructor(private readonly doctorProvider: DoctorProvider) {}
 
   @OnEvent(SharedEvents.CREATE_DOCTOR, { async: true })
   async createDoctor(ctx: CreateDoctor) {
-    const result = this.doctorProvider.createDoctor(ctx);
-    return await this.errorHandler.handleResult(
-      result,
-      DOCTOR_SUCCESS_MESSAGES.DOCTOR_CREATED,
-    );
+    return this.doctorProvider.createDoctor(ctx);
   }
 }
