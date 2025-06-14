17e597ca733135c60a99c022b3d5f159905fc1c7
3illBaby <thrillanon@outlook.com>
2025-06-14 21:40:25 +0100

    Add HTTP status code to IdentityError

--- src/modules/identity/error/identity.error.ts
+++ src/modules/identity/error/identity.error.ts
@@ -1,10 +1,14 @@
+import { HttpStatus } from '@nestjs/common';
 import { IDENTITY_ERROR_MESSAGES } from '../data/identity.data';
 
 export class IdentityError extends Error {
+  public status: number;
   constructor(
     message: string = IDENTITY_ERROR_MESSAGES.ERROR_STORING_IDENTIFICATION,
+    status: number = HttpStatus.INTERNAL_SERVER_ERROR,
   ) {
     super(message);
     this.name = 'IdentityError';
+    this.status = status;
   }
 }
