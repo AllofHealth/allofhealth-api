becdaa8b4e4896f324de81a9dfd110d149b29b78
3illBaby <thrillanon@outlook.com>
2025-06-14 21:41:36 +0100

    Simplify user creation error handling

--- src/modules/user/service/user.service.ts
+++ src/modules/user/service/user.service.ts
@@ -1,23 +1,12 @@
-import { ErrorHandler } from '@/shared/error-handler/error.handler';
 import { Injectable } from '@nestjs/common';
-import { ResultAsync } from 'neverthrow';
-import { USER_SUCCESS_MESSAGE } from '../data/user.data';
-import { UserError } from '../error/user.error';
 import { ICreateUser } from '../interface/user.interface';
 import { UserProvider } from '../provider/user.provider';
 
 @Injectable()
 export class UserService {
-  private handler: ErrorHandler;
-  constructor(private readonly userProvider: UserProvider) {
-    this.handler = new ErrorHandler();
-  }
+  constructor(private readonly userProvider: UserProvider) {}
 
-  createUser(ctx: ICreateUser) {
-    const result = ResultAsync.fromPromise(
-      this.userProvider.createUser(ctx),
-      (error: Error) => new UserError(error.message),
-    );
-    return this.handler.handleResult(result, USER_SUCCESS_MESSAGE.USER_CREATED);
+  async createUser(ctx: ICreateUser) {
+    return await this.userProvider.createUser(ctx);
   }
 }
