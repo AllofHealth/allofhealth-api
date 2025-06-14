c55f7f5a8ea708a0b4c9206eb99d53481c3c675a
3illBaby <thrillanon@outlook.com>
2025-06-15 00:21:03 +0100

    Add AuthError class for auth error handling

---
+++ src/modules/auth/error/auth.error.ts
@@ -0,0 +1,13 @@
+import { HttpStatus } from '@nestjs/common';
+import { AuthErrorMessage } from '../data/auth.data';
+
+export class AuthError extends Error {
+  public status: number;
+  constructor(
+    message: string = AuthErrorMessage.REGISTRATION_FAILED,
+    status: number = HttpStatus.INTERNAL_SERVER_ERROR,
+  ) {
+    super(message);
+    this.status = status;
+  }
+}
