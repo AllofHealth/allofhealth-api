d04352068cc36d535e853c7673ed00ff76e59eec
3illBaby <thrillanon@outlook.com>
2025-06-14 21:40:53 +0100

    Remove error handling from identity service

--- src/modules/identity/service/identity.service.ts
+++ src/modules/identity/service/identity.service.ts
@@ -1,24 +1,15 @@
 import { StoreId } from '@/shared/dtos/event.dto';
-import { ErrorHandler } from '@/shared/error-handler/error.handler';
 import { SharedEvents } from '@/shared/events/shared.events';
 import { Injectable } from '@nestjs/common';
 import { OnEvent } from '@nestjs/event-emitter';
-import { IDENTITY_SUCCESS_MESSAGES } from '../data/identity.data';
 import { IdentityProvider } from '../provider/identity.provider';
 
 @Injectable()
 export class IdentityService {
-  private handler: ErrorHandler;
-  constructor(private readonly identityProvider: IdentityProvider) {
-    this.handler = new ErrorHandler();
-  }
+  constructor(private readonly identityProvider: IdentityProvider) {}
 
   @OnEvent(SharedEvents.STORE_IDENTIFICATION)
-  storeIdentity(ctx: StoreId) {
-    const result = this.identityProvider.storeId(ctx);
-    return this.handler.handleResult(
-      result,
-      IDENTITY_SUCCESS_MESSAGES.SUCCESS_STORING_IDENTIFICATION,
-    );
+  async storeIdentity(ctx: StoreId) {
+    return this.identityProvider.storeId(ctx);
   }
 }
