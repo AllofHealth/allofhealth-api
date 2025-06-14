65f8733a94a28e9c1eabf2efa16436d7e96de60b
3illBaby <thrillanon@outlook.com>
2025-06-14 21:41:14 +0100

    Add user fetch and delete response messages

--- src/modules/user/data/user.data.ts
+++ src/modules/user/data/user.data.ts
@@ -3,8 +3,12 @@ export enum USER_ERROR_MESSAGES {
   EMAIL_EXIST = 'Email already exist please log in',
   USER_NOT_FOUND = 'User not found',
   USER_EXISTS = 'User already exists',
+  ERROR_FETCHING_USER = 'Error fetching user',
+  ERROR_DELETING_USER = 'Error deleting user',
 }
 
 export enum USER_SUCCESS_MESSAGE {
   USER_CREATED = 'User created successfully',
+  USER_FETCHED_SUCCESSFULLY = 'User fetched successfully',
+  USER_DELETED_SUCCESSFULLY = 'User deleted successfully',
 }
