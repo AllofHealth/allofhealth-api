27957568fde080e1b458ceb03f653d32f9517357
3illBaby <thrillanon@outlook.com>
2025-06-15 18:02:42 +0100

    Add UPDATE_USER_LOGIN event constant

--- src/shared/events/shared.events.ts
+++ src/shared/events/shared.events.ts
@@ -3,4 +3,5 @@ export enum SharedEvents {
   DELETE_USER = 'delete.user',
   STORE_IDENTIFICATION = 'store.identification',
   CREATE_SMART_ACCOUNT = 'create.smart.account',
+  UPDATE_USER_LOGIN = 'update.user.login',
 }
