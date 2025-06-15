33802f71ac5726ac7146245d0354125b6b760a67
3illBaby <thrillanon@outlook.com>
2025-06-15 20:55:30 +0100

    Update user.data.ts

--- src/modules/user/data/user.data.ts
+++ src/modules/user/data/user.data.ts
@@ -7,11 +7,11 @@ export enum USER_ERROR_MESSAGES {
   ERROR_DELETING_USER = 'Error deleting user',
   ERROR_UPDATING_USER = 'Error updating user',
   INVALID_ROLE = 'Invalid role',
-  USER_UPDATED = 'User updated successfully',
 }
 
 export enum USER_SUCCESS_MESSAGE {
   USER_CREATED = 'User created successfully',
   USER_FETCHED_SUCCESSFULLY = 'User fetched successfully',
   USER_DELETED_SUCCESSFULLY = 'User deleted successfully',
+  USER_UPDATED = 'User updated successfully',
 }
