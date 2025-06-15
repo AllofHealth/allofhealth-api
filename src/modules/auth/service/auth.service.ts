96f3becf8da4b90314e27a198a98d3119ea32ba4
3illBaby <thrillanon@outlook.com>
2025-06-15 18:02:09 +0100

    Add login handler to auth service

--- src/modules/auth/service/auth.service.ts
+++ src/modules/auth/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { ICreateUser } from '@/modules/user/interface/user.interface';
 import { Injectable } from '@nestjs/common';
 import { AuthProvider } from '../provider/auth.provider';
+import { ILogin } from '../interface/auth.interface';
 
 @Injectable()
 export class AuthService {
@@ -8,5 +9,9 @@ export class AuthService {
 
   async handleRegister(ctx: ICreateUser) {
     return await this.authProvider.handleSignup(ctx);
+  }
+
+  async handleLogin(ctx: ILogin) {
+    return await this.authProvider.handleLogin(ctx);
   }
 }
