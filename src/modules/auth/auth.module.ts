51559029d2730ce049f99b7861e2819009f65ab0
3illBaby <thrillanon@outlook.com>
2025-06-15 15:56:39 +0100

    Add AuthController and export AuthService in auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,9 +3,12 @@ import { AuthProvider } from './provider/auth.provider';
 import { AuthService } from './service/auth.service';
 import { TokenModule } from '../token/token.module';
 import { UserModule } from '../user/user.module';
+import { AuthController } from './controller/auth.controller';
 
 @Module({
   imports: [UserModule, TokenModule],
   providers: [AuthProvider, AuthService],
+  controllers: [AuthController],
+  exports: [AuthService],
 })
 export class AuthModule {}
