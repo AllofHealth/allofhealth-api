80400ae2c76e1caa057415f8e4325f8247839efb
3illBaby <thrillanon@outlook.com>
2025-06-15 18:01:43 +0100

    Add AuthUtils provider to auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,13 +1,14 @@
+import { AuthUtils } from '@/shared/utils/auth.utils';
 import { Module } from '@nestjs/common';
-import { AuthProvider } from './provider/auth.provider';
-import { AuthService } from './service/auth.service';
 import { TokenModule } from '../token/token.module';
 import { UserModule } from '../user/user.module';
 import { AuthController } from './controller/auth.controller';
+import { AuthProvider } from './provider/auth.provider';
+import { AuthService } from './service/auth.service';
 
 @Module({
   imports: [UserModule, TokenModule],
-  providers: [AuthProvider, AuthService],
+  providers: [AuthProvider, AuthService, AuthUtils],
   controllers: [AuthController],
   exports: [AuthService],
 })
