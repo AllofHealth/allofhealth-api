7880aad739d42085c184e36ac9e8b5e003eaf308
3illBaby <thrillanon@outlook.com>
2025-06-16 17:24:34 +0100

    chore(asset.module.ts): fixed imports and export asste service

--- src/modules/asset/asset.module.ts
+++ src/modules/asset/asset.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AssetService } from './asset/asset.service';
-import { Asset } from './asset/asset';
-import { AssetService } from './asset.service';
-import { Asset } from './asset';
-import { AssetController } from './asset.controller';
+import { AssetProvider } from './provider/asset.provider';
+import { AssetService } from './service/asset.service';
+import { AssetController } from './controller/asset.controller';
 
 @Module({
-  providers: [AssetService, Asset],
-  controllers: [AssetController]
+  providers: [AssetService, AssetProvider],
+  controllers: [AssetController],
+  exports: [AssetService],
 })
 export class AssetModule {}
