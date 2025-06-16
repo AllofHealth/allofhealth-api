de88cb87ad64b7165d12b559e18fadc5d21ccf71
3illBaby <thrillanon@outlook.com>
2025-06-16 20:07:30 +0100

    Add AssetModule to user module imports

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -5,9 +5,10 @@ import { UserController } from './controller/user.controller';
 import { AuthUtils } from '@/shared/utils/auth.utils';
 import { AccountQueueModule } from '@/shared/queues/account/account-queue.module';
 import { TokenModule } from '../token/token.module';
+import { AssetModule } from '../asset/asset.module';
 
 @Module({
-  imports: [AccountQueueModule, forwardRef(() => TokenModule)],
+  imports: [AccountQueueModule, forwardRef(() => TokenModule), AssetModule],
   providers: [UserProvider, UserService, AuthUtils],
   controllers: [UserController],
   exports: [UserService],
