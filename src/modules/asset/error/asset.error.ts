e137c912601c0e879cd94e0410f6dd291418d071
3illBaby <thrillanon@outlook.com>
2025-06-16 17:20:00 +0100

    Add custom AssetError class

---
+++ src/modules/asset/error/asset.error.ts
@@ -0,0 +1,13 @@
+import { HttpStatus } from '@nestjs/common';
+
+export class AssetError extends Error {
+  public status: number;
+  constructor(
+    message: string,
+    status: number = HttpStatus.INTERNAL_SERVER_ERROR,
+  ) {
+    super(message);
+    this.status = status;
+    this.name = 'AssetError';
+  }
+}
