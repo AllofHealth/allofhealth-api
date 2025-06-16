de0cfe01b8e470ab805f5e99457153c9beeb7365
3illBaby <thrillanon@outlook.com>
2025-06-16 17:21:11 +0100

    Make identification fields optional and add file IDs

--- src/modules/identity/interface/identity.interface.ts
+++ src/modules/identity/interface/identity.interface.ts
@@ -2,7 +2,9 @@ import { TRole } from '@/shared/interface/shared.interface';
 
 export interface IStoreIdentification {
   userId: string;
-  governmentId: string;
+  governmentFileId?: string;
+  governmentId?: string;
+  scannedLicenseFileId?: string;
   scannedLicense?: string;
   role: TRole;
 }
