abac5db25fd244f9a5d767afcac5c6c46df5a9c7
3illBaby <thrillanon@outlook.com>
2025-06-29 17:17:34 +0100

    feat(external-account.provider.ts): implemented function to provide signer

--- src/shared/modules/external-account/provider/external-account.provider.ts
+++ src/shared/modules/external-account/provider/external-account.provider.ts
@@ -2,13 +2,26 @@ import {
   LISK_MAINNET_RPC_URL,
   LISK_TESTNET_RPC_URL,
 } from '@/shared/data/constants';
-import { Injectable } from '@nestjs/common';
+import { DRIZZLE_PROVIDER } from '@/shared/drizzle/drizzle.provider';
+import { Database } from '@/shared/drizzle/drizzle.types';
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { ethers } from 'ethers';
+import * as schema from '@/schemas/schema';
+import { eq } from 'drizzle-orm';
+import { AuthUtils } from '@/shared/utils/auth.utils';
+import { BiconomyConfig } from '@/shared/config/biconomy/biconomy.config';
+import { ErrorHandler } from '@/shared/error-handler/error.handler';
+import { ExternalAccountErrorMessage } from '../data/external-account.data';
 
 @Injectable()
 export class ExternalAccountProvider {
   private readonly rpcUrl: string;
-  constructor() {
+  constructor(
+    @Inject(DRIZZLE_PROVIDER) private readonly db: Database,
+    private readonly authUtils: AuthUtils,
+    private readonly config: BiconomyConfig,
+    private readonly handlerService: ErrorHandler,
+  ) {
     this.rpcUrl =
       process.env.NODE_ENV === 'production'
         ? LISK_MAINNET_RPC_URL
@@ -34,6 +47,45 @@ export class ExternalAccountProvider {
         walletAddress: wallet.walletAddress,
       },
     };
+  }
+
+  async provideSigner(userId: string) {
+    try {
+      const result = await this.db
+        .select({
+          privateKey: schema.accounts.privateKey,
+        })
+        .from(schema.accounts)
+        .where(eq(schema.accounts.userId, userId));
+
+      if (
+        !result ||
+        result.length === 0 ||
+        !result[0].privateKey ||
+        result[0].privateKey === null
+      ) {
+        throw new HttpException(
+          `No signature found for this account`,
+          HttpStatus.NOT_FOUND,
+        );
+      }
+
+      const decryptedPrivateKey = this.authUtils.decryptKey({
+        data: result[0].privateKey,
+        key: this.config.ENCRYPTION_KEY,
+      });
+
+      const signer = new ethers.Wallet(
+        decryptedPrivateKey,
+        this.handleGetProvider(),
+      );
+      return signer;
+    } catch (e) {
+      return this.handlerService.handleError(
+        e,
+        ExternalAccountErrorMessage.ERROR_PROVIDING_SIGNER,
+      );
+    }
   }
 
   handleCreateWallet() {
