ab5f125af9c98ce7bae7da5aa310f4a2761db025
3illBaby <thrillanon@outlook.com>
2025-06-29 18:25:28 +0100

    feat(contract.provider.ts): impl function to provide signed contract instance

--- src/modules/contract/provider/contract.provider.ts
+++ src/modules/contract/provider/contract.provider.ts
@@ -1,4 +1,30 @@
+import { ContractConfig } from '@/shared/config/smart-contract/contract.config';
+import { ExternalAccountService } from '@/shared/modules/external-account/service/external-account.service';
 import { Injectable } from '@nestjs/common';
+import { ABI } from '../data/contract.data';
+import { ethers } from 'ethers';
 
 @Injectable()
-export class ContractProvider {}
+export class ContractProvider {
+  constructor(
+    private readonly contractConfig: ContractConfig,
+    private readonly eoaService: ExternalAccountService,
+  ) {}
+
+  private provideABI() {
+    return ABI;
+  }
+
+  private async provideSigner(userId: string) {
+    return this.eoaService.provideSigner(userId);
+  }
+
+  async provideContract(userId: string) {
+    const signer = await this.provideSigner(userId);
+    return new ethers.Contract(
+      this.contractConfig.CONTRACT_ADDRESS,
+      this.provideABI(),
+      signer,
+    );
+  }
+}
