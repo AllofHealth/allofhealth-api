06f47206d6b29f91a1b8ae301cdc10b426503e3f
3illBaby <thrillanon@outlook.com>
2025-06-29 16:47:08 +0100

    feat(auth.utils.ts): implement function to encrypt and decrypt key

--- src/shared/utils/auth.utils.ts
+++ src/shared/utils/auth.utils.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { SALT_ROUNDS } from '../data/constants';
+import { IEncrypt } from '../interface/shared.interface';
+import * as crypto from 'crypto';
 
 interface ICompare {
   password: string;
@@ -15,5 +17,42 @@ export class AuthUtils {
 
   async compare(args: ICompare) {
     return await bcrypt.compare(args.password, args.hashedPassword);
+  }
+
+  encryptKey(ctx: IEncrypt) {
+    const { data, key } = ctx;
+    const iv = crypto.randomBytes(16);
+    const BufferKey = Buffer.from(key, 'base64');
+
+    const cipher = crypto.createCipheriv('aes-256-cbc', BufferKey, iv);
+
+    let encrypted = cipher.update(data);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+
+    return `${iv.toString('hex')}:${encrypted.toString('hex')}`;
+  }
+
+  decryptKey(ctx: IEncrypt) {
+    const { data, key } = ctx;
+    const textParts = data.split(':');
+
+    if (textParts.length < 2) {
+      throw new Error('Invalid encrypted data format');
+    }
+
+    const ivHex = textParts.shift();
+    if (!ivHex) {
+      throw new Error('Invalid IV in encrypted data');
+    }
+
+    const iv = Buffer.from(ivHex, 'hex');
+    const encryptedText = Buffer.from(textParts.join(':'), 'hex');
+
+    const BufferKey = Buffer.from(key, 'base64');
+
+    const decipher = crypto.createDecipheriv('aes-256-cbc', BufferKey, iv);
+    let decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return decrypted.toString();
   }
 }
