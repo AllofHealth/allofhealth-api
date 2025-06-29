96ba36b38883152b0e01fcd1de0d0edc8265d0f5
3illBaby <thrillanon@outlook.com>
2025-06-29 17:39:53 +0100

    Add providers and imports to ContractModule

--- src/modules/contract/contract.module.ts
+++ src/modules/contract/contract.module.ts
@@ -1,4 +1,10 @@
+import { ExternalAccountModule } from '@/shared/modules/external-account/external-account.module';
 import { Module } from '@nestjs/common';
+import { ContractService } from './service/contract.service';
+import { ContractProvider } from './provider/contract.provider';
 
-@Module({})
+@Module({
+  imports: [ExternalAccountModule],
+  providers: [ContractService, ContractProvider],
+})
 export class ContractModule {}
