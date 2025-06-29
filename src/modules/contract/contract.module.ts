479b1a487ed488ed408cfff4167e5872257e83cf
3illBaby <thrillanon@outlook.com>
2025-06-29 20:29:52 +0100

    Add ErrorHandler provider to ContractModule

--- src/modules/contract/contract.module.ts
+++ src/modules/contract/contract.module.ts
@@ -2,9 +2,10 @@ import { ExternalAccountModule } from '@/shared/modules/external-account/externa
 import { Module } from '@nestjs/common';
 import { ContractService } from './service/contract.service';
 import { ContractProvider } from './provider/contract.provider';
+import { ErrorHandler } from '@/shared/error-handler/error.handler';
 
 @Module({
   imports: [ExternalAccountModule],
-  providers: [ContractService, ContractProvider],
+  providers: [ContractService, ContractProvider, ErrorHandler],
 })
 export class ContractModule {}
