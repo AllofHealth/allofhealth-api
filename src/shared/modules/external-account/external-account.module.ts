9dbea02cd92619dae20b711a43f7bf332a6807c1
3illBaby <thrillanon@outlook.com>
2025-06-29 17:17:01 +0100

    chore(external-account.module.ts): provided auth utils and error handler

--- src/shared/modules/external-account/external-account.module.ts
+++ src/shared/modules/external-account/external-account.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ExternalAccountService } from './service/external-account.service';
 import { ExternalAccountProvider } from './provider/external-account.provider';
+import { AuthUtils } from '@/shared/utils/auth.utils';
+import { ErrorHandler } from '@/shared/error-handler/error.handler';
 
 @Module({
-  providers: [ExternalAccountService, ExternalAccountProvider],
+  providers: [
+    ExternalAccountService,
+    ExternalAccountProvider,
+    AuthUtils,
+    ErrorHandler,
+  ],
   exports: [ExternalAccountService],
 })
 export class ExternalAccountModule {}
