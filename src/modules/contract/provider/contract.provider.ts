c9081d95ae825a2705d37afb60d2409994fd34be
3illBaby <thrillanon@outlook.com>
2025-06-29 20:30:18 +0100

    Add admin contract instance and system admin count

--- src/modules/contract/provider/contract.provider.ts
+++ src/modules/contract/provider/contract.provider.ts
@@ -1,14 +1,20 @@
 import { ContractConfig } from '@/shared/config/smart-contract/contract.config';
 import { ExternalAccountService } from '@/shared/modules/external-account/service/external-account.service';
-import { Injectable } from '@nestjs/common';
-import { ABI } from '../data/contract.data';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import {
+  ABI,
+  ContractErrorMessages,
+  ContractSuccessMessages,
+} from '../data/contract.data';
 import { ethers } from 'ethers';
+import { ErrorHandler } from '@/shared/error-handler/error.handler';
 
 @Injectable()
 export class ContractProvider {
   constructor(
     private readonly contractConfig: ContractConfig,
     private readonly eoaService: ExternalAccountService,
+    private readonly handlerService: ErrorHandler,
   ) {}
 
   private provideABI() {
@@ -19,6 +25,14 @@ export class ContractProvider {
     return this.eoaService.provideSigner(userId);
   }
 
+  provideAdminContractInstance() {
+    return new ethers.Contract(
+      this.contractConfig.CONTRACT_ADDRESS,
+      this.provideABI(),
+      this.eoaService.provideAdminSigner(),
+    );
+  }
+
   async provideContract(userId: string) {
     const signer = await this.provideSigner(userId);
     return new ethers.Contract(
@@ -26,5 +40,22 @@ export class ContractProvider {
       this.provideABI(),
       signer,
     );
+  }
+
+  async handleGetSystemAdminCount() {
+    try {
+      const contract = this.provideAdminContractInstance();
+      const count = await contract.systemAdminCount();
+      return this.handlerService.handleReturn({
+        status: HttpStatus.OK,
+        message: ContractSuccessMessages.TX_EXECUTED_SUCCESSFULLY,
+        data: Number(count),
+      });
+    } catch (e) {
+      return this.handlerService.handleError(
+        e,
+        ContractErrorMessages.ERROR_PROVIDING_SYSTEM_ADMIN_COUNT,
+      );
+    }
   }
 }
