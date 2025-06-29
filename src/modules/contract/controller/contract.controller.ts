d766fa9c1b7d46b688cc908dfbf90f95c13b5dc3
3illBaby <thrillanon@outlook.com>
2025-06-30 00:52:31 +0100

    Add patient registration and count endpoints

--- src/modules/contract/controller/contract.controller.ts
+++ src/modules/contract/controller/contract.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Query } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { ContractService } from '../service/contract.service';
 
@@ -10,5 +10,17 @@ export class ContractController {
   @Get('system-admin-count')
   async systemAdminCount() {
     return await this.contractService.systemAdminCount();
+  }
+
+  @Get('patientCount')
+  async patientCount() {
+    return await this.contractService.getPatientCount();
+  }
+
+  @Post('registerPatient')
+  async registerPatient(@Query('userId') userId: string) {
+    return await this.contractService.registerPatient({
+      userId,
+    });
   }
 }
