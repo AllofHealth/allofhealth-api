c3e1a1b7bc4004fb00f99e82d3bdb56671f2dc1b
3illBaby <thrillanon@outlook.com>
2025-06-29 22:06:03 +0100

    chore(account-abstaction.data.ts): extended data

--- src/shared/modules/account-abstraction/data/account-abstraction.data.ts
+++ src/shared/modules/account-abstraction/data/account-abstraction.data.ts
@@ -1,8 +1,11 @@
 export enum AccountAbstractionSuccessMessage {
   SMART_ACCOUNT_CREATED = 'Smart account created successfully',
+  SMART_ADDRESS_FOUND = 'Smart address found successfully',
 }
 
 export enum AccountAbstractionErrorMessage {
   ERROR_CREATING_SMART_ACCOUNT = 'Error creating smart account',
   ERROR_CREATING_SMART_ACCOUNT_CLIENT = 'Error creating smart account client',
+  ERROR_GETTING_SMART_ADDRESS = 'Error getting smart address',
+  ERROR_SMART_ADDRESS_NOT_FOUND = 'Smart address not found',
 }
