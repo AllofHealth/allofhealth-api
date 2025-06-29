dbf0d15bcd808ff9ae2d3515d24ac7a808d40a7d
3illBaby <thrillanon@outlook.com>
2025-06-29 21:43:34 +0100

    chore(contract.mdoule.ts): registered controller

--- src/modules/contract/contract.module.ts
+++ src/modules/contract/contract.module.ts
@@ -1,11 +1,13 @@
+import { ErrorHandler } from '@/shared/error-handler/error.handler';
 import { ExternalAccountModule } from '@/shared/modules/external-account/external-account.module';
 import { Module } from '@nestjs/common';
-import { ContractService } from './service/contract.service';
+import { ContractController } from './controller/contract.controller';
 import { ContractProvider } from './provider/contract.provider';
-import { ErrorHandler } from '@/shared/error-handler/error.handler';
+import { ContractService } from './service/contract.service';
 
 @Module({
   imports: [ExternalAccountModule],
   providers: [ContractService, ContractProvider, ErrorHandler],
+  controllers: [ContractController],
 })
 export class ContractModule {}
