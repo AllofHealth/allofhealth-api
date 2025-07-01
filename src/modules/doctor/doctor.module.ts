c08fd256cda31b33a84a52b4b0ef0ee4aef7c986
3illBaby <thrillanon@outlook.com>
2025-07-01 15:06:21 +0100

    Add ErrorHandler provider to DoctorModule

--- src/modules/doctor/doctor.module.ts
+++ src/modules/doctor/doctor.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { DoctorProvider } from './provider/doctor.provider';
 import { DoctorService } from './service/doctor.service';
 import { DoctorController } from './controller/doctor.controller';
+import { ErrorHandler } from '@/shared/error-handler/error.handler';
 
 @Module({
-  providers: [DoctorService, DoctorProvider],
+  providers: [DoctorService, DoctorProvider, ErrorHandler],
   controllers: [DoctorController],
 })
 export class DoctorModule {}
