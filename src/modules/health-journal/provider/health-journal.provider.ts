8d176997bd82257362e27771a14a9f867ed60c15
3illBaby <thrillanon@outlook.com>
2025-07-01 21:25:32 +0100

    feat(health-journal.provider.ts): impl func to add entry to journal

--- src/modules/health-journal/provider/health-journal.provider.ts
+++ src/modules/health-journal/provider/health-journal.provider.ts
@@ -1,4 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { DRIZZLE_PROVIDER } from '@/shared/drizzle/drizzle.provider';
+import { Database } from '@/shared/drizzle/drizzle.types';
+import { HttpStatus, Inject, Injectable } from '@nestjs/common';
+import * as schema from '@/schemas/schema';
+import { ErrorHandler } from '@/shared/error-handler/error.handler';
+import {
+  HealthJournalErrorMessages as HEM,
+  HealthJournalSuccessMessages as HSM,
+} from '../data/health-journal.data';
+import { IAddEntry } from '../interface/health-journal.interface';
 
 @Injectable()
-export class HealthJournalProvider {}
+export class HealthJournalProvider {
+  constructor(
+    @Inject(DRIZZLE_PROVIDER) private readonly db: Database,
+    private readonly handler: ErrorHandler,
+  ) {}
+
+  async addJournalEntry(ctx: IAddEntry) {
+    const { userId, mood, activities, symptoms, tags } = ctx;
+    try {
+      const journal = await this.db
+        .insert(schema.health_journal)
+        .values({
+          userId,
+          mood: mood.toLowerCase(),
+          activities,
+          symptoms,
+          tags,
+        })
+        .returning();
+
+      if (!journal || journal.length === 0) {
+        return this.handler.handleReturn({
+          status: HttpStatus.BAD_REQUEST,
+          message: HEM.ERROR_ADDING_ENTRY,
+        });
+      }
+
+      return this.handler.handleReturn({
+        status: HttpStatus.OK,
+        message: HSM.SUCCESS_ADDING_ENTRY,
+      });
+    } catch (e) {
+      return this.handler.handleError(e, HEM.ERROR_ADDING_ENTRY);
+    }
+  }
+}
