0102435ba6e059cc523bd88acaadac3d64f24b72
3illBaby <thrillanon@outlook.com>
2025-07-01 15:41:05 +0100

    Add circular dependency with TokenModule

--- src/modules/doctor/doctor.module.ts
+++ src/modules/doctor/doctor.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { DoctorProvider } from './provider/doctor.provider';
 import { DoctorService } from './service/doctor.service';
 import { DoctorController } from './controller/doctor.controller';
 import { ErrorHandler } from '@/shared/error-handler/error.handler';
+import { TokenModule } from '../token/token.module';
 
 @Module({
+  imports: [forwardRef(() => TokenModule)],
   providers: [DoctorService, DoctorProvider, ErrorHandler],
   controllers: [DoctorController],
 })
