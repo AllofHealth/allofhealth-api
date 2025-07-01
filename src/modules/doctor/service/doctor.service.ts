f72ce0d645c4c3572cf8593f2bc0a3300c02385f
3illBaby <thrillanon@outlook.com>
2025-07-01 15:06:50 +0100

    Add fetchAllDoctors method to DoctorService

--- src/modules/doctor/service/doctor.service.ts
+++ src/modules/doctor/service/doctor.service.ts
@@ -3,6 +3,7 @@ import { SharedEvents } from '@/shared/events/shared.events';
 import { Injectable } from '@nestjs/common';
 import { OnEvent } from '@nestjs/event-emitter';
 import { DoctorProvider } from '../provider/doctor.provider';
+import { IFetchDoctors } from '../interface/doctor.interface';
 
 @Injectable()
 export class DoctorService {
@@ -10,6 +11,10 @@ export class DoctorService {
 
   @OnEvent(SharedEvents.CREATE_DOCTOR, { async: true })
   async createDoctor(ctx: CreateDoctor) {
-    return this.doctorProvider.createDoctor(ctx);
+    return await this.doctorProvider.createDoctor(ctx);
+  }
+
+  async fetchAllDoctors(ctx: IFetchDoctors) {
+    return await this.doctorProvider.fetchAllDoctors(ctx);
   }
 }
