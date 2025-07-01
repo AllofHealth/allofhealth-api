3c63f1f572f237423581cc5a98515dfbaeeb338b
3illBaby <thrillanon@outlook.com>
2025-07-01 15:41:35 +0100

    Remove createAccount endpoint from AA controller

--- src/shared/modules/account-abstraction/controller/account-abstraction.controller.ts
+++ src/shared/modules/account-abstraction/controller/account-abstraction.controller.ts
@@ -1,13 +1,7 @@
-import { Controller, Post, Query } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AccountAbstractionService } from '../service/account-abstraction.service';
 
 @Controller('account-abstraction')
 export class AccountAbstractionController {
   constructor(private readonly service: AccountAbstractionService) {}
-  @Post('createAccount')
-  async createAccount(@Query('userId') userId: string) {
-    return await this.service.createSmartAccount({
-      userId,
-    });
-  }
 }
