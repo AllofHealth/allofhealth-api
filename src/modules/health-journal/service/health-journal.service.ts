eefe886bcf82dc342331f82e03a9154a7d9693e3
3illBaby <thrillanon@outlook.com>
2025-07-01 21:27:33 +0100

    feat(health-journal.service.ts): consumed provider

--- src/modules/health-journal/service/health-journal.service.ts
+++ src/modules/health-journal/service/health-journal.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { HealthJournalProvider } from '../provider/health-journal.provider';
+import { IAddEntry } from '../interface/health-journal.interface';
 
 @Injectable()
-export class HealthJournalService {}
+export class HealthJournalService {
+  constructor(private readonly journalProvider: HealthJournalProvider) {}
+
+  async addEntryToJournal(ctx: IAddEntry) {
+    return await this.journalProvider.addJournalEntry(ctx);
+  }
+}
