d8ae43e717e055a787fc61f1199f40ed8afac3b2
3illBaby <thrillanon@outlook.com>
2025-07-01 15:06:12 +0100

    Add error and success messages for fetching doctors

--- src/modules/doctor/data/doctor.data.ts
+++ src/modules/doctor/data/doctor.data.ts
@@ -2,8 +2,10 @@ export enum DOCTOR_ERROR_MESSGAES {
   ERROR_CREATING_DOCTOR = 'Error creating doctor',
   DOCTOR_NOT_FOUND = 'Doctor not found',
   DOCTOR_ALREADY_EXISTS = 'Doctor already exists',
+  ERROR_FETCHING_ALL_DOCTORS = 'Error fetching all doctors',
 }
 
 export enum DOCTOR_SUCCESS_MESSAGES {
   DOCTOR_CREATED = 'Doctor created successfully',
+  DOCTOR_FETCHED = 'Doctor fetched successfully',
 }
