8ed1d024870b449e13532652712e39400b874260
3illBaby <thrillanon@outlook.com>
2025-07-01 21:24:58 +0100

    fix(health-journal.module.ts): resolved broken imports

--- src/modules/health-journal/health-journal.module.ts
+++ src/modules/health-journal/health-journal.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { HealthJournal } from './health-journal';
-import { HealthJournalController } from './health-journal.controller';
-import { HealthJournalService } from './health-journal.service';
+import { HealthJournalProvider } from './provider/health-journal.provider';
+import { HealthJournalService } from './service/health-journal.service';
+import { HealthJournalController } from './controller/health-journal.controller';
+import { ErrorHandler } from '@/shared/error-handler/error.handler';
 
 @Module({
-  providers: [HealthJournalService, HealthJournal],
+  providers: [HealthJournalService, HealthJournalProvider, ErrorHandler],
   controllers: [HealthJournalController],
 })
 export class HealthJournalModule {}
