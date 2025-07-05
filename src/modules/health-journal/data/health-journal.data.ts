86a07a22c49546c4c6a9e954d4e2878b07f32929
3illBaby <thrillanon@outlook.com>
2025-07-05 07:36:16 +0100

    chore(health-journal.data.ts): added error and success messages

--- src/modules/health-journal/data/health-journal.data.ts
+++ src/modules/health-journal/data/health-journal.data.ts
@@ -1,7 +1,9 @@
 export enum HealthJournalErrorMessages {
   ERROR_ADDING_ENTRY = 'Error adding entry',
+  ERROR_FETCHING_JOURNAL = 'Error fetching journal',
 }
 
 export enum HealthJournalSuccessMessages {
   SUCCESS_ADDING_ENTRY = 'Entry added successfully',
+  SUCCESS_FETCHING_JOURNAL = 'Journal fetched successfully',
 }
