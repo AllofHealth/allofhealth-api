6fac0df364eb550befb63ce5f32a4a093caf72e5
3illBaby <thrillanon@outlook.com>
2025-07-06 00:22:46 +0100

    Add contract interfaces for record access control

--- src/modules/contract/interface/contract.interface.ts
+++ src/modules/contract/interface/contract.interface.ts
@@ -1,3 +1,5 @@
+export type TAccess = 'full' | 'read' | 'write';
+
 export interface IHandleApproveAccessToAddNewRecord {
   userId: string;
   doctorId: string;
@@ -6,4 +8,32 @@ export interface IHandleApproveAccessToAddNewRecord {
 export interface IApprovedToAddNewRecord {
   patientId: number;
   doctorAddress: string;
+}
+
+export interface IApproveRecordAccess {
+  practitionerId: string;
+  userId: string;
+  recordId: number;
+  duration: number;
+}
+
+export interface IViewerHasAccessToRecords {
+  practitionerAddress: string;
+  patientId: number;
+  recordId: number;
+}
+
+export interface IApproveRecordAccessTx {
+  practitionerAddress: string;
+  patientChainId: number;
+  recordChainId: number;
+  duration?: number;
+}
+
+export interface IHandleApproval {
+  userId: string;
+  practitionerId: string;
+  recordId?: number;
+  duration?: number;
+  accessLevel: TAccess;
 }
