662008409c6deea5f6460f08697a78716812058e
3illBaby <thrillanon@outlook.com>
2025-07-05 07:47:52 +0100

    Add fetch journal method to health journal service

--- src/modules/health-journal/service/health-journal.service.ts
+++ src/modules/health-journal/service/health-journal.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { HealthJournalProvider } from '../provider/health-journal.provider';
-import { IAddEntry } from '../interface/health-journal.interface';
+import {
+  IAddEntry,
+  IFetchJournal,
+} from '../interface/health-journal.interface';
 
 @Injectable()
 export class HealthJournalService {
@@ -8,5 +11,9 @@ export class HealthJournalService {
 
   async addEntryToJournal(ctx: IAddEntry) {
     return await this.journalProvider.addJournalEntry(ctx);
+  }
+
+  async fetchUserJournals(ctx: IFetchJournal) {
+    return await this.journalProvider.fetchUserJournal(ctx);
   }
 }
