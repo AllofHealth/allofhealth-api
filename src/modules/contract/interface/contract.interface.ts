7299abcbc1b83b251835c8e01cdafe1707f1e801
3illBaby <thrillanon@outlook.com>
2025-07-05 14:15:00 +0100

    Add interface for approved record access

--- src/modules/contract/interface/contract.interface.ts
+++ src/modules/contract/interface/contract.interface.ts
@@ -2,3 +2,8 @@ export interface IHandleApproveAccessToAddNewRecord {
   userId: string;
   doctorId: string;
 }
+
+export interface IApprovedToAddNewRecord {
+  patientId: number;
+  doctorAddress: string;
+}
