d281fb4d239507ecb2da228ff283f36bb366adc8
3illBaby <thrillanon@outlook.com>
2025-07-06 21:31:40 +0100

    chore(approval.module.ts): import user module

--- src/modules/approval/approval.module.ts
+++ src/modules/approval/approval.module.ts
@@ -6,12 +6,14 @@ import { ErrorHandler } from '@/shared/error-handler/error.handler';
 import { AccountAbstractionModule } from '@/shared/modules/account-abstraction/account-abstraction.module';
 import { ContractModule } from '../contract/contract.module';
 import { TokenModule } from '../token/token.module';
+import { UserModule } from '../user/user.module';
 
 @Module({
   imports: [
     forwardRef(() => AccountAbstractionModule),
     forwardRef(() => ContractModule),
     forwardRef(() => TokenModule),
+    forwardRef(() => UserModule),
   ],
   providers: [ApprovalService, ApprovalProvider, ErrorHandler],
   controllers: [ApprovalController],
