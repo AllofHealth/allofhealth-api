342fa5229a484c9c3a9b747aed78481361ce4885
3illBaby <thrillanon@outlook.com>
2025-07-06 21:08:51 +0100

    feat(approval.service.ts): consumed provider

--- src/modules/approval/service/approval.service.ts
+++ src/modules/approval/service/approval.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ApprovalProvider } from '../provider/approval.provider';
+import { IHandleApproval } from '@/modules/contract/interface/contract.interface';
 
 @Injectable()
-export class ApprovalService {}
+export class ApprovalService {
+  constructor(private readonly approvalProvider: ApprovalProvider) {}
+
+  async createApproval(ctx: IHandleApproval) {
+    return await this.approvalProvider.createApproval(ctx);
+  }
+}
