eb0d7569ef216d3887ee42a45f582360ac13c54b
3illBaby <thrillanon@outlook.com>
2025-07-06 19:02:47 +0100

    chore(approval.module.ts): imported modules

--- src/modules/approval/approval.module.ts
+++ src/modules/approval/approval.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { ApprovalService } from './service/approval.service';
 import { ApprovalProvider } from './provider/approval.provider';
 import { ApprovalController } from './controller/approval.controller';
+import { ErrorHandler } from '@/shared/error-handler/error.handler';
+import { AccountAbstractionModule } from '@/shared/modules/account-abstraction/account-abstraction.module';
+import { ContractModule } from '../contract/contract.module';
 
 @Module({
-  providers: [ApprovalService, ApprovalProvider],
+  imports: [
+    forwardRef(() => AccountAbstractionModule),
+    forwardRef(() => ContractModule),
+  ],
+  providers: [ApprovalService, ApprovalProvider, ErrorHandler],
   controllers: [ApprovalController],
 })
 export class ApprovalModule {}
