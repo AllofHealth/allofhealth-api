ca150c0cc6661b24d38549244c0bae452c682b0c
3illBaby <thrillanon@outlook.com>
2025-07-06 19:03:02 +0100

    chore(approval.data.ts): extended data

---
+++ src/modules/approval/data/approval.data.ts
@@ -0,0 +1,10 @@
+export enum APPROVAL_ERROR_MESSAGE {
+  ERROR_CREATING_APPROVAL = 'Error creating approval',
+  RECORD_ID_IS_REQUIRED = 'Record ID is required',
+  ERROR_VERIFYING_PRACTITIONER = 'Error verifying practitioner',
+  NOT_A_VALID_PRACTITIONER = 'Not a valid practitioner',
+}
+
+export enum APPROVAL_SUCCESS_MESSAGE {
+  APPROVAL_CREATED = 'Approval created successfully',
+}
