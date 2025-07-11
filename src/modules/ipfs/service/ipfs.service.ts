b74b618fa05560cb6015aab1f30cbc034d282317
3illBaby <thrillanon@outlook.com>
2025-07-11 07:31:57 +0100

    Add IPFS provider and client implementation

--- src/modules/ipfs/service/ipfs.service.ts
+++ src/modules/ipfs/service/ipfs.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { IpfsProvider } from '../provider/ipfs.provider';
 
 @Injectable()
-export class IpfsService {}
+export class IpfsService {
+  constructor(private readonly ipfsProvider: IpfsProvider) {}
+
+  async uploadRecord() {
+    return this.ipfsProvider.uploadRecord();
+  }
+}
