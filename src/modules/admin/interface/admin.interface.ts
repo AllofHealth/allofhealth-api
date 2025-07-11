b0b71f6de7966213779d1cb6f4bac853113bf8f9
3illBaby <thrillanon@outlook.com>
2025-07-12 00:20:18 +0100

    Add admin interface types and permission levels

--- src/modules/admin/interface/admin.interface.ts
+++ src/modules/admin/interface/admin.interface.ts
@@ -1,1 +1,8 @@
-export interface Admin {}
+export type TPermissionLevel = 'super' | 'system';
+
+export interface ICreateAdmin {
+  userName: string;
+  email: string;
+  password: string;
+  permissionLevel?: TPermissionLevel;
+}
