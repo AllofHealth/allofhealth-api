aab9c730c0d55460353b6ba66ce6e60726dca0c0
3illBaby <thrillanon@outlook.com>
2025-07-11 16:53:06 +0100

    chore(approval.data.ts): added approval data

--- src/modules/approval/data/approval.data.ts
+++ src/modules/approval/data/approval.data.ts
@@ -3,8 +3,11 @@ export enum APPROVAL_ERROR_MESSAGE {
   RECORD_ID_IS_REQUIRED = 'Record ID is required',
   ERROR_VERIFYING_PRACTITIONER = 'Error verifying practitioner',
   NOT_A_VALID_PRACTITIONER = 'Not a valid practitioner',
+  ERROR_FETCHING_DOCTOR_APPROVAL = 'Error fetching doctor approval',
+  APPROVALS_NOT_FOUND = 'Approvals not found',
 }
 
 export enum APPROVAL_SUCCESS_MESSAGE {
   APPROVAL_CREATED = 'Approval created successfully',
+  APPROVAL_FETCHED = 'Approval fetched successfully',
 }
