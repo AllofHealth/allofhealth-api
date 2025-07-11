1995385cdf10052e478fca7f0213d3d02f187b39
3illBaby <thrillanon@outlook.com>
2025-07-11 23:53:02 +0100

    chore(admin.module.ts): registered admin module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,25 @@
+import config from '@/shared/config/config';
 import { ConfigifyModule } from '@itgorillaz/configify';
 import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { EventEmitterModule } from '@nestjs/event-emitter';
-import { ScheduleModule } from '@nestjs/schedule';
 import { JwtModule } from '@nestjs/jwt';
-import config from '@/shared/config/config';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApprovalModule } from './modules/approval/approval.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { ContractModule } from './modules/contract/contract.module';
 import { DoctorModule } from './modules/doctor/doctor.module';
 import { HealthJournalModule } from './modules/health-journal/health-journal.module';
 import { IdentityModule } from './modules/identity/identity.module';
+import { IpfsModule } from './modules/ipfs/ipfs.module';
 import { MyLoggerModule } from './modules/my-logger/my-logger.module';
 import { UserModule } from './modules/user/user.module';
 import { DrizzleModule } from './shared/drizzle/drizzle.module';
 import { AccountAbstractionModule } from './shared/modules/account-abstraction/account-abstraction.module';
 import { ExternalAccountModule } from './shared/modules/external-account/external-account.module';
-import { ApprovalModule } from './modules/approval/approval.module';
-import { IpfsModule } from './modules/ipfs/ipfs.module';
 
 @Module({
   imports: [
@@ -66,6 +66,7 @@ import { IpfsModule } from './modules/ipfs/ipfs.module';
     HealthJournalModule,
     ApprovalModule,
     IpfsModule,
+    AdminModule,
   ],
   controllers: [AppController],
   providers: [AppService],
