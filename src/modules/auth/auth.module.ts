eccbea9ee3efa6834577123a7640e99c43abad7e
3illBaby <thrillanon@outlook.com>
2025-07-12 00:20:34 +0100

    Add ErrorHandler provider to Auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -5,10 +5,11 @@ import { UserModule } from '../user/user.module';
 import { AuthController } from './controller/auth.controller';
 import { AuthProvider } from './provider/auth.provider';
 import { AuthService } from './service/auth.service';
+import { ErrorHandler } from '@/shared/error-handler/error.handler';
 
 @Module({
   imports: [UserModule, TokenModule],
-  providers: [AuthProvider, AuthService, AuthUtils],
+  providers: [AuthProvider, AuthService, AuthUtils, ErrorHandler],
   controllers: [AuthController],
   exports: [AuthService],
 })
