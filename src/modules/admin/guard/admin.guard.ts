58dcd937799200119c7ccc36ea5becabac2f61d2
3illBaby <thrillanon@outlook.com>
2025-07-12 00:03:13 +0100

    feat(admin.guard.ts): implemented simple admin guard

--- src/modules/admin/guard/admin.guard.ts
+++ src/modules/admin/guard/admin.guard.ts
@@ -1,11 +1,38 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { DRIZZLE_PROVIDER } from '@/shared/drizzle/drizzle.provider';
+import { Database } from '@/shared/drizzle/drizzle.types';
+import {
+  CanActivate,
+  ExecutionContext,
+  HttpException,
+  HttpStatus,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { eq } from 'drizzle-orm';
+import * as schema from '@/schemas/schema';
 
 @Injectable()
 export class AdminGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+  constructor(@Inject(DRIZZLE_PROVIDER) private readonly db: Database) {}
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const userId = request.query.userId;
+
+    try {
+      const admin = await this.db.query.admin.findFirst({
+        where: eq(schema.admin.id, userId),
+      });
+
+      if (!admin || typeof admin === undefined) {
+        throw new UnauthorizedException();
+      }
+      return true;
+    } catch (e) {
+      throw new HttpException(
+        'Error validating admin status',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
