b6b64f1777259d94e3212b68f0512bcb07c56680
3illBaby <thrillanon@outlook.com>
2025-07-11 23:52:08 +0100

    chore(admin.module.ts): init admin module

---
+++ src/modules/admin/data/admin.data.ts
@@ -0,0 +1,7 @@
+export enum ADMIN_ERROR_MESSAGES {
+  ERROR_CREATING_ADMIN = 'Error creating admin',
+}
+
+export const ADMIN_SUCCESS_MESSAGES = {
+  SUCCESS_CREATING_ADMIN: 'Admin created successfully',
+};
