7dba88245892610ee6666dccc90d159fa720e114
3illBaby <thrillanon@outlook.com>
2025-07-13 00:44:14 +0100

    Add UserModule and TokenModule imports to AdminModule

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -1,11 +1,14 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { AdminProvider } from './provider/admin.provider';
 import { AdminService } from './service/admin.service';
 import { AdminController } from './controller/admin.controller';
 import { ErrorHandler } from '@/shared/error-handler/error.handler';
 import { AuthUtils } from '@/shared/utils/auth.utils';
+import { UserModule } from '../user/user.module';
+import { TokenModule } from '../token/token.module';
 
 @Module({
+  imports: [UserModule, forwardRef(() => TokenModule)],
   providers: [AdminProvider, AdminService, ErrorHandler, AuthUtils],
   controllers: [AdminController],
 })
