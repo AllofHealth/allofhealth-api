94b98dacbd2a9e871c1c41b11765551589f1a943
3illBaby <thrillanon@outlook.com>
2025-07-12 11:44:05 +0100

    Add admin related error and success messages

--- src/modules/admin/data/admin.data.ts
+++ src/modules/admin/data/admin.data.ts
@@ -1,7 +1,16 @@
 export enum ADMIN_ERROR_MESSAGES {
   ERROR_CREATING_ADMIN = 'Error creating admin',
+  ERROR_FINDING_ADMIN = 'Error finding admin',
+  ERROR_UPDATING_ADMIN = 'Error updating admin',
+  ERROR_DELETING_ADMIN = 'Error deleting admin',
+  ERROR_UPDATING_ADMIN_PERMISSIONS = 'Error updating admin permissions',
+  ADMIN_NOT_FOUND = 'Admin not found',
+  ADMIN_EXISTS = 'admin exists, please login',
+  ERROR_VALIDATING_SUPER_ADMIN = 'Error validating super admin',
 }
 
-export const ADMIN_SUCCESS_MESSAGES = {
-  SUCCESS_CREATING_ADMIN: 'Admin created successfully',
-};
+export enum ADMIN_SUCCESS_MESSAGES {
+  SUCCESS_CREATING_ADMIN = 'Admin created successfully',
+  ADMIN_FOUND = 'Admin found successfully',
+  SUPER_ADMIN_CREATED = 'Super admin created successfully',
+}
