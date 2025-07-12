bac26d55255074dcef8858596be22593a0ed16f4
3illBaby <thrillanon@outlook.com>
2025-07-12 11:44:31 +0100

    Add admin creation methods to AdminService

--- src/modules/admin/service/admin.service.ts
+++ src/modules/admin/service/admin.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { ICreateAdmin, ICreateSystemAdmin } from '../interface/admin.interface';
+import { AdminProvider } from '../provider/admin.provider';
 
 @Injectable()
-export class AdminService {}
+export class AdminService {
+  constructor(private readonly adminProvider: AdminProvider) {}
+
+  async createSuperAdmin(ctx: ICreateAdmin) {
+    return await this.adminProvider.createSuperAdmin(ctx);
+  }
+
+  async createSystemAdmin(ctx: ICreateSystemAdmin) {
+    return await this.adminProvider.createSystemAdmin(ctx);
+  }
+}
