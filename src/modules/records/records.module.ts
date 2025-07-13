79a2b023fa79f787c4cc194831469bd9d18cc858
3illBaby <thrillanon@outlook.com>
2025-07-13 21:54:26 +0100

    Add DoctorModule and ErrorHandler to Records module

--- src/modules/records/records.module.ts
+++ src/modules/records/records.module.ts
@@ -1,10 +1,13 @@
+import { ErrorHandler } from '@/shared/error-handler/error.handler';
 import { Module } from '@nestjs/common';
+import { DoctorModule } from '../doctor/doctor.module';
+import { RecordsController } from './controller/records.controller';
 import { RecordsProvider } from './provider/records.provider';
 import { RecordsService } from './service/records.service';
-import { RecordsController } from './controller/records.controller';
 
 @Module({
-  providers: [RecordsProvider, RecordsService],
+  imports: [DoctorModule],
+  providers: [RecordsProvider, RecordsService, ErrorHandler],
   controllers: [RecordsController],
 })
 export class RecordsModule {}
