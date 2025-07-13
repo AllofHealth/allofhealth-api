cc6df91f3a65f18c98ff0c5a1c8d7b3f86a0280c
3illBaby <thrillanon@outlook.com>
2025-07-13 01:05:39 +0100

    Replace UserModule with AuthModule in admin imports

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -4,11 +4,11 @@ import { AdminService } from './service/admin.service';
 import { AdminController } from './controller/admin.controller';
 import { ErrorHandler } from '@/shared/error-handler/error.handler';
 import { AuthUtils } from '@/shared/utils/auth.utils';
-import { UserModule } from '../user/user.module';
 import { TokenModule } from '../token/token.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [UserModule, forwardRef(() => TokenModule)],
+  imports: [AuthModule, forwardRef(() => TokenModule)],
   providers: [AdminProvider, AdminService, ErrorHandler, AuthUtils],
   controllers: [AdminController],
 })
