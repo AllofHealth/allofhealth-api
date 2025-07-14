34b7203e0bcb4fa4a498a8d3deaf0ecba9afca0d
3illBaby <thrillanon@outlook.com>
2025-07-14 23:04:04 +0100

    Add encryption/decryption record status messages

--- src/modules/records/data/records.data.ts
+++ src/modules/records/data/records.data.ts
@@ -2,8 +2,12 @@ export enum RECORDS_ERROR_MESSAGES {
   RECORD_NOT_FOUND = 'Record not found',
   RECORD_ALREADY_EXISTS = 'Record already exists',
   ERROR_CREATING_RECORD = 'Error creating record',
+  ERROR_ENCRYPTING_RECORD = 'Error encrypting record',
+  ERROR_DECRYPTING_RECORD = 'Error decrypting record',
 }
 
 export const RECORDS_SUCCESS_MESSAGES = {
   SUCCESS_CREATING_RECORD: 'Record created successfully',
+  RECORD_ENCRYPTED_SUCCESSFULLY: 'Record encrypted successfully',
+  RECORD_DECRYPTED_SUCCESSFULLY: 'Record decrypted successfully',
 };
