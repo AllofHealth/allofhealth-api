f39f27b230fff015cccb65aeca192e97e833ad64
3illBaby <thrillanon@outlook.com>
2025-07-14 22:16:15 +0100

    chore(records.module.ts): registered encryption service

--- src/modules/records/records.module.ts
+++ src/modules/records/records.module.ts
@@ -4,10 +4,16 @@ import { DoctorModule } from '../doctor/doctor.module';
 import { RecordsController } from './controller/records.controller';
 import { RecordsProvider } from './provider/records.provider';
 import { RecordsService } from './service/records.service';
+import { RecordsEncryptionService } from './service/record-encryption.service';
 
 @Module({
   imports: [DoctorModule],
-  providers: [RecordsProvider, RecordsService, ErrorHandler],
+  providers: [
+    RecordsProvider,
+    RecordsService,
+    ErrorHandler,
+    RecordsEncryptionService,
+  ],
   controllers: [RecordsController],
 })
 export class RecordsModule {}
