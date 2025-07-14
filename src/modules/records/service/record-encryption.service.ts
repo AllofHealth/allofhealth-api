6d9f42c2bfcb8deae2418aea6a58107d80d57c95
3illBaby <thrillanon@outlook.com>
2025-07-14 22:38:17 +0100

    Add encryption/decryption methods to RecordsEncryptionService

--- src/modules/records/service/record-encryption.service.ts
+++ src/modules/records/service/record-encryption.service.ts
@@ -1,4 +1,25 @@
+import { RecordConfig } from '@/shared/config/record/record.config';
+import { AuthUtils } from '@/shared/utils/auth.utils';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class RecordsEncryptionService {}
+export class RecordsEncryptionService {
+  constructor(
+    private readonly authUtils: AuthUtils,
+    private readonly recordConfig: RecordConfig,
+  ) {}
+
+  encryptTest(test: string) {
+    return this.authUtils.encryptKey({
+      data: test,
+      key: this.recordConfig.RECORD_ENCRYPTION_KEY,
+    });
+  }
+
+  decryptTest(encryptedText: string) {
+    return this.authUtils.decryptKey({
+      data: encryptedText,
+      key: this.recordConfig.RECORD_ENCRYPTION_KEY,
+    });
+  }
+}
