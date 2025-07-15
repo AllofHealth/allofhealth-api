087c1312bcf22bc9a67e72de6bf1c056391752cd
3illBaby <thrillanon@outlook.com>
2025-07-15 23:20:25 +0100

    feat(otp.provider.ts): impl func to generate otp

--- src/modules/otp/provider/otp.provider.ts
+++ src/modules/otp/provider/otp.provider.ts
@@ -1,4 +1,23 @@
+import { ErrorHandler } from '@/shared/error-handler/error.handler';
 import { Injectable } from '@nestjs/common';
+import * as OTPAuth from 'otpauth';
 
 @Injectable()
-export class OtpProvider {}
+export class OtpProvider {
+  constructor(private readonly handler: ErrorHandler) {}
+
+  initTotp() {
+    return new OTPAuth.TOTP({
+      issuer: 'allofhealth',
+      label: 'allofhealth-api',
+      algorithm: 'SHA1',
+      digits: 6,
+      period: 120,
+    });
+  }
+
+  generateOtp() {
+    const totp = this.initTotp();
+    return totp.generate();
+  }
+}
