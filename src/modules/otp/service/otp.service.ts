b53f08cfc052e2e12c6a1380b4b58c0f95383439
3illBaby <thrillanon@outlook.com>
2025-07-15 23:54:11 +0100

    Add OTP validation and secret generation methods

--- src/modules/otp/service/otp.service.ts
+++ src/modules/otp/service/otp.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { OtpProvider } from '../provider/otp.provider';
+import * as OTPAuth from 'otpauth';
 
 @Injectable()
 export class OtpService {
@@ -7,5 +8,21 @@ export class OtpService {
 
   generateOtp() {
     return this.otpProvider.generateOtp();
+  }
+
+  validateOtp(token: string) {
+    return this.otpProvider.validateOtp(token);
+  }
+
+  generateOtpWithSecret(secret: OTPAuth.Secret | string) {
+    return this.otpProvider.generateOtpWithSecret(secret);
+  }
+
+  validateOtpWithSecret(token: string, secret: OTPAuth.Secret | string) {
+    return this.otpProvider.validateOtpWithSecret(token, secret);
+  }
+
+  generateSecret() {
+    return this.otpProvider.generateSecret();
   }
 }
