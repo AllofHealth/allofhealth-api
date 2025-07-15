3f6ae7abdba44be8f56ff22d7488ae8e20abf65b
3illBaby <thrillanon@outlook.com>
2025-07-15 23:20:49 +0100

    test(otp.service.spec.ts): added test case to generate otp

--- src/modules/otp/service/otp.service.spec.ts
+++ src/modules/otp/service/otp.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OtpService } from './otp.service';
+import { ErrorHandler } from '@/shared/error-handler/error.handler';
 
 describe('OtpService', () => {
   let service: OtpService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OtpService],
+      providers: [OtpService, ErrorHandler],
     }).compile();
 
     service = module.get<OtpService>(OtpService);
@@ -14,5 +15,14 @@ describe('OtpService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('Otp', () => {
+    it('Should generate a 6 digit otp string', () => {
+      const otp = service.generateOtp();
+
+      console.debug(otp);
+      expect(otp).toHaveLength(6);
+    });
   });
 });
