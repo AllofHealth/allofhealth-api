773c66b9ecb0e9c19be98bbc3f8142925716282c
3illBaby <thrillanon@outlook.com>
2025-07-15 23:20:00 +0100

    chore(otp.data.ts): added otp constanst

---
+++ src/modules/otp/data/otp.data.ts
@@ -0,0 +1,9 @@
+export enum OTP_ERROR_MESSAGES {
+  ERROR_CREATING_OTP = 'Error creating OTP',
+  ERROR_VALIDATING_OTP = 'Error validating OTP',
+}
+
+export enum OTP_SUCCESS_MESSAGES {
+  SUCCESS_CREATING_OTP = 'OTP created successfully',
+  SUCCESS_VALIDATING_OTP = 'OTP validated successfully',
+}
