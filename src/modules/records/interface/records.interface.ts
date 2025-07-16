3c9ea5a14fa8e82435dbb45dedd925c37de35c8e
3illBaby <thrillanon@outlook.com>
2025-07-16 23:30:22 +0100

    Update attachment types to Express.Multer.File

--- src/modules/records/interface/records.interface.ts
+++ src/modules/records/interface/records.interface.ts
@@ -1,9 +1,9 @@
 export interface ICreateRecord extends IEncryptRecord {
   practitionerId: string;
   patientId: string;
-  attachment1?: File;
-  attachment2?: File;
-  attachment3?: File;
+  attachment1?: Express.Multer.File;
+  attachment2?: Express.Multer.File;
+  attachment3?: Express.Multer.File;
 }
 
 export interface IEncryptRecord {
