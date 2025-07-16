f8da0bc58c9cd0fc564d72815611c809eccd2413
3illBaby <thrillanon@outlook.com>
2025-07-16 21:41:33 +0100

    Add file attachments to record creation interface

--- src/modules/records/interface/records.interface.ts
+++ src/modules/records/interface/records.interface.ts
@@ -1,7 +1,10 @@
-export interface ICreateRecord {
+export interface ICreateRecord extends IEncryptRecord {
   title: string;
   practitionerId: string;
   patientId: string;
+  attachment1?: File;
+  attachment2?: File;
+  attachment3?: File;
 }
 
 export interface IEncryptRecord {
