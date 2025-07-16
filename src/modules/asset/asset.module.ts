c34275a2a8edbcf392d3245d06c385640e24ed85
3illBaby <thrillanon@outlook.com>
2025-07-16 11:54:02 +0100

    Add FileCleanupService to Asset module

--- src/modules/asset/asset.module.ts
+++ src/modules/asset/asset.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AssetController } from './controller/asset.controller';
 import { AssetProvider } from './provider/asset.provider';
 import { AssetService } from './service/asset.service';
+import { FileCleanupService } from './tasks/file-cleanup.service';
 
 @Module({
-  providers: [AssetService, AssetProvider],
+  providers: [AssetService, AssetProvider, FileCleanupService],
   controllers: [AssetController],
   exports: [AssetService],
 })
