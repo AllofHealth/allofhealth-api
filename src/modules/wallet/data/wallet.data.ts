6cc41b90571a56b61f9abd5458fcda0cf5641ed5
3illBaby <thrillanon@outlook.com>
2025-07-17 23:10:54 +0100

    chore(wallet.data.ts): extended wallet data

--- src/modules/wallet/data/wallet.data.ts
+++ src/modules/wallet/data/wallet.data.ts
@@ -1,7 +1,7 @@
 export enum WALLET_ERROR_MESSAGES {
   ERROR_FETCHING_WALLET = 'Error fetching wallet',
+  NOT_FOUND = 'Wallet not found',
 }
-
 
 export enum WALLET_SUCCESS_MESSAGES {
   SUCCESS_FETCHING_WALLET = 'Successfully fetched wallet',
