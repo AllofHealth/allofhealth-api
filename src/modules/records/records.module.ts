f35080e707747aacb4e6b127e83f510720f87636
3illBaby <thrillanon@outlook.com>
2025-07-17 14:45:38 +0100

    chore(records.module.ts): import contract module

--- src/modules/records/records.module.ts
+++ src/modules/records/records.module.ts
@@ -1,16 +1,22 @@
 import { ErrorHandler } from '@/shared/error-handler/error.handler';
 import { AuthUtils } from '@/shared/utils/auth.utils';
 import { forwardRef, Module } from '@nestjs/common';
+import { ApprovalModule } from '../approval/approval.module';
+import { ContractModule } from '../contract/contract.module';
 import { DoctorModule } from '../doctor/doctor.module';
+import { IpfsModule } from '../ipfs/ipfs.module';
 import { RecordsController } from './controller/records.controller';
 import { RecordsProvider } from './provider/records.provider';
 import { RecordsEncryptionService } from './service/record-encryption.service';
 import { RecordsService } from './service/records.service';
-import { IpfsModule } from '../ipfs/ipfs.module';
-import { ApprovalModule } from '../approval/approval.module';
 
 @Module({
-  imports: [DoctorModule, IpfsModule, forwardRef(() => ApprovalModule)],
+  imports: [
+    DoctorModule,
+    IpfsModule,
+    forwardRef(() => ApprovalModule),
+    forwardRef(() => ContractModule),
+  ],
   providers: [
     RecordsProvider,
     RecordsService,
