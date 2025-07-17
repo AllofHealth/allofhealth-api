c6ce531869995a3893afb251ae320a4d5148d6b6
3illBaby <thrillanon@outlook.com>
2025-07-17 23:11:26 +0100

    feat(wallet.provider.st): impl function to fetch wallet balance

--- src/modules/wallet/provider/wallet.provider.ts
+++ src/modules/wallet/provider/wallet.provider.ts
@@ -1,8 +1,14 @@
+import * as schema from '@/schemas/schema';
 import { DRIZZLE_PROVIDER } from '@/shared/drizzle/drizzle.provider';
 import { Database } from '@/shared/drizzle/drizzle.types';
 import { ErrorHandler } from '@/shared/error-handler/error.handler';
 import { ExternalAccountService } from '@/shared/modules/external-account/service/external-account.service';
-import { Inject, Injectable } from '@nestjs/common';
+import { HttpStatus, Inject, Injectable } from '@nestjs/common';
+import { eq } from 'drizzle-orm';
+import {
+  WALLET_ERROR_MESSAGES as WEM,
+  WALLET_SUCCESS_MESSAGES as WSM,
+} from '../data/wallet.data';
 
 @Injectable()
 export class WalletProvider {
@@ -11,4 +17,39 @@ export class WalletProvider {
     private readonly eoaService: ExternalAccountService,
     private readonly handler: ErrorHandler,
   ) {}
+
+  async fetchUserWallet(userId: string) {
+    try {
+      const wallet = await this.db
+        .select({
+          smartWalletAddress: schema.accounts.smartWalletAddress,
+          updatedAt: schema.accounts.updatedAt,
+        })
+        .from(schema.accounts)
+        .where(eq(schema.accounts.userId, userId));
+
+      if (!wallet || wallet.length === 0) {
+        return this.handler.handleReturn({
+          status: HttpStatus.NOT_FOUND,
+          message: WEM.NOT_FOUND,
+        });
+      }
+
+      const walletBalance = await this.eoaService.getBalance(
+        wallet[0].smartWalletAddress,
+      );
+
+      return this.handler.handleReturn({
+        status: HttpStatus.OK,
+        message: WSM.SUCCESS_FETCHING_WALLET,
+        data: {
+          walletAddress: wallet[0].smartWalletAddress,
+          balance: walletBalance,
+          lastUpdated: wallet[0].updatedAt,
+        },
+      });
+    } catch (e) {
+      return this.handler.handleError(e, WEM.ERROR_FETCHING_WALLET);
+    }
+  }
 }
