85c50b4f09f95800bd30bdc7ebf5af934ca4cb33
3illBaby <thrillanon@outlook.com>
2025-07-17 22:55:03 +0100

    Add dependencies to WalletProvider constructor

--- src/modules/wallet/provider/wallet.provider.ts
+++ src/modules/wallet/provider/wallet.provider.ts
@@ -1,4 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { DRIZZLE_PROVIDER } from '@/shared/drizzle/drizzle.provider';
+import { Database } from '@/shared/drizzle/drizzle.types';
+import { ErrorHandler } from '@/shared/error-handler/error.handler';
+import { ExternalAccountService } from '@/shared/modules/external-account/service/external-account.service';
+import { Inject, Injectable } from '@nestjs/common';
 
 @Injectable()
-export class WalletProvider { }
+export class WalletProvider {
+  constructor(
+    @Inject(DRIZZLE_PROVIDER) private readonly db: Database,
+    private readonly eoaService: ExternalAccountService,
+    private readonly handler: ErrorHandler,
+  ) {}
+}
