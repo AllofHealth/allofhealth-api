43f9946411f5b3013fa938079f1f0829ae1da7eb
3illBaby <thrillanon@outlook.com>
2025-07-18 16:25:11 +0100

    chore(health-info.module.ts): imported guard module

--- src/modules/health-info/health-info.module.ts
+++ src/modules/health-info/health-info.module.ts
@@ -4,10 +4,16 @@ import { HealthInfoService } from './service/health-info.service';
 import { HealthInfoController } from './controller/health-info.controller';
 import { UserModule } from '../user/user.module';
 import { TokenModule } from '../token/token.module';
+import { ApprovalModule } from '../approval/approval.module';
+import { ErrorHandler } from '@/shared/error-handler/error.handler';
 
 @Module({
-  imports: [forwardRef(() => UserModule), forwardRef(() => TokenModule)],
-  providers: [HealthInfoProvider, HealthInfoService],
+  imports: [
+    forwardRef(() => UserModule),
+    forwardRef(() => TokenModule),
+    forwardRef(() => ApprovalModule),
+  ],
+  providers: [HealthInfoProvider, HealthInfoService, ErrorHandler],
   controllers: [HealthInfoController],
   exports: [HealthInfoService],
 })
