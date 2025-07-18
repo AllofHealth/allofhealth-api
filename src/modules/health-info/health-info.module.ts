89359cd3c0bdbe931cd12d7e5f53ac8a3498127b
3illBaby <thrillanon@outlook.com>
2025-07-18 17:14:30 +0100

    Reorder import statements alphabetically

--- src/modules/health-info/health-info.module.ts
+++ src/modules/health-info/health-info.module.ts
@@ -1,12 +1,12 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { HealthInfoProvider } from './provider/health-info.provider';
 import { HealthInfoService } from './service/health-info.service';
-import { HealthInfoController } from './controller/health-info.controller';
 import { UserModule } from '../user/user.module';
 import { TokenModule } from '../token/token.module';
 import { ApprovalModule } from '../approval/approval.module';
 import { ErrorHandler } from '@/shared/error-handler/error.handler';
 import { AssetModule } from '../asset/asset.module';
+import { HealthInfoController } from './controller/health-info.controller';
 
 @Module({
   imports: [
