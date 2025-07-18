39419e3ae2eee565e187e4da6df810aaa6d3288a
3illBaby <thrillanon@outlook.com>
2025-07-18 16:25:32 +0100

    chore(health-info.interface.ts): added interface

--- src/modules/health-info/interface/health-info.interface.ts
+++ src/modules/health-info/interface/health-info.interface.ts
@@ -1,1 +1,11 @@
-export interface HealthInfo {}
+export type TPainLevel = 'severe' | 'moderate' | 'mild';
+
+export interface ICreateHealthInfo {
+  userId: string;
+  howAreYouFeeling: string;
+  whenDidItStart: string;
+  painLevel: TPainLevel;
+  knownConditions?: string[];
+  medicationsTaken?: string[];
+  attachmentFilePath?: string;
+}
