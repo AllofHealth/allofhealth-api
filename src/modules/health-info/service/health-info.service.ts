f7ddef151dc68180d0b4e064c187535e84ab418f
3illBaby <thrillanon@outlook.com>
2025-07-18 16:57:59 +0100

    feat(health-info.service.ts): consumed provider

--- src/modules/health-info/service/health-info.service.ts
+++ src/modules/health-info/service/health-info.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { HealthInfoProvider } from '../provider/health-info.provider';
+import { ICreateHealthInfo } from '../interface/health-info.interface';
 
 @Injectable()
-export class HealthInfoService {}
+export class HealthInfoService {
+  constructor(private readonly healthInfoProvider: HealthInfoProvider) {}
+
+  async createHealthInfo(ctx: ICreateHealthInfo) {
+    return await this.healthInfoProvider.createHealthInformation(ctx);
+  }
+}
