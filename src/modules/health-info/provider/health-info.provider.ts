37de5fe770bb1b4dc068b7c5a3a17c42da616925
3illBaby <thrillanon@outlook.com>
2025-07-18 17:47:25 +0100

    Add update health information endpoint

--- src/modules/health-info/provider/health-info.provider.ts
+++ src/modules/health-info/provider/health-info.provider.ts
@@ -12,6 +12,7 @@ import {
 import {
   ICreateHealthInfo,
   IHandleAttachmentUpload,
+  IUpdateHealthInfo,
 } from '../interface/health-info.interface';
 import {
   HEALTH_INFO_ERROR_MESSAGES as HEM,
@@ -19,6 +20,7 @@ import {
 } from '../data/health-info.data';
 import { AssetService } from '@/modules/asset/service/asset.service';
 import * as schema from '@/schemas/schema';
+import { eq } from 'drizzle-orm';
 
 @Injectable()
 export class HealthInfoProvider {
@@ -50,6 +52,49 @@ export class HealthInfoProvider {
         `Error uploading attachment, ${e}`,
       );
     }
+  }
+
+  private async prepareRecordToUpdate(ctx: IUpdateHealthInfo) {
+    const {
+      userId,
+      howAreYouFeeling,
+      whenDidItStart,
+      painLevel,
+      knownConditions,
+      medicationsTaken,
+      attachmentFilePath,
+    } = ctx;
+
+    const dataToUpdate: Record<string, any> = {};
+
+    if (howAreYouFeeling) {
+      dataToUpdate.howAreYouFeeling = howAreYouFeeling;
+    }
+    if (whenDidItStart) {
+      dataToUpdate.whenDidItStart = whenDidItStart;
+    }
+    if (painLevel) {
+      dataToUpdate.painLevel = painLevel;
+    }
+    if (knownConditions) {
+      dataToUpdate.knownConditions = knownConditions;
+    }
+    if (medicationsTaken) {
+      dataToUpdate.medicationsTaken = medicationsTaken;
+    }
+    if (attachmentFilePath) {
+      const { url, fileId } = await this.handleAttachmentUpload({
+        userId: ctx.userId,
+        attachmentFilePath: attachmentFilePath,
+      });
+
+      dataToUpdate.attachment = url;
+      dataToUpdate.attachmentFileId = fileId;
+    }
+
+    dataToUpdate.updatedAt = new Date();
+
+    return dataToUpdate;
   }
 
   async createHealthInformation(ctx: ICreateHealthInfo) {
@@ -92,6 +137,24 @@ export class HealthInfoProvider {
       });
     } catch (e) {
       return this.handler.handleError(e, HEM.ERROR_CREATING_HEALTH_INFO);
+    }
+  }
+
+  async updateHealthInformation(ctx: IUpdateHealthInfo) {
+    try {
+      const dataToUpdate = await this.prepareRecordToUpdate(ctx);
+
+      await this.db
+        .update(schema.healthInformation)
+        .set(dataToUpdate)
+        .where(eq(schema.healthInformation.userId, ctx.userId));
+
+      return this.handler.handleReturn({
+        status: HttpStatus.OK,
+        message: HSM.HEALTH_INFO_UPDATED,
+      });
+    } catch (e) {
+      return this.handler.handleError(e, HEM.ERROR_UPDATING_HEALTH_INFO);
     }
   }
 }
