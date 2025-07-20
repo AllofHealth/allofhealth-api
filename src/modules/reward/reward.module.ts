d80d7d0248d6b76e4d1092362f4b6e1965a7a6a2
3illBaby <thrillanon@outlook.com>
2025-07-20 17:39:42 +0100

    Add RewardDistributionService to reward module

--- src/modules/reward/reward.module.ts
+++ src/modules/reward/reward.module.ts
@@ -3,10 +3,16 @@ import { RewardProvider } from './provider/reward.provider';
 import { RewardService } from './service/reward.service';
 import { ErrorHandler } from '@/shared/error-handler/error.handler';
 import { MintQueueModule } from '@/shared/queues/mint/mint-queue.module';
+import { RewardDistributionService } from './tasks/reward-distrubtion.task';
 
 @Module({
   imports: [MintQueueModule],
-  providers: [RewardProvider, RewardService, ErrorHandler],
+  providers: [
+    RewardProvider,
+    RewardService,
+    ErrorHandler,
+    RewardDistributionService,
+  ],
   exports: [RewardService],
 })
 export class RewardModule {}
