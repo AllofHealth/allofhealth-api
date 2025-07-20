cf0fed55642bb800ab44c938e6c450ff7dde5eeb
3illBaby <thrillanon@outlook.com>
2025-07-20 16:51:26 +0100

    chore(reward.data.ts): added reward data

---
+++ src/modules/reward/data/reward.data.ts
@@ -0,0 +1,10 @@
+export enum REWARD_ERROR_MESSAGES {
+  NOT_FOUND = 'Reward not found',
+  ERROR_CREATING_REWARD = 'Error creating reward',
+  ERROR_UPDATING_REWARD = 'Error updating reward',
+}
+
+export enum REWARD_SUCCESS_MESSAGES {
+  REWARD_CREATED = 'Reward created successfully',
+  REWARD_UPDATED = 'Reward updated successfully',
+}
