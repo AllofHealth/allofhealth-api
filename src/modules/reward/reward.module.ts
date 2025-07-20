af188abe1933a7cb8e7259b91c3cf222e5da4a23
3illBaby <thrillanon@outlook.com>
2025-07-20 16:51:51 +0100

    chore(reward.module.ts): import error handler

--- src/modules/reward/reward.module.ts
+++ src/modules/reward/reward.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RewardProvider } from './provider/reward.provider';
 import { RewardService } from './service/reward.service';
+import { ErrorHandler } from '@/shared/error-handler/error.handler';
 
 @Module({
-  providers: [RewardProvider, RewardService],
+  providers: [RewardProvider, RewardService, ErrorHandler],
+  exports: [RewardService],
 })
 export class RewardModule {}
