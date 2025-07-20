b849adb3ef5ec267378438536f2c70983e199f29
3illBaby <thrillanon@outlook.com>
2025-07-20 17:48:09 +0100

    Add reward service methods for minting and daily counts

--- src/modules/reward/service/reward.service.ts
+++ src/modules/reward/service/reward.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { RewardProvider } from '../provider/reward.provider';
+import { EUpdateTaskCount } from '@/shared/dtos/event.dto';
 
 @Injectable()
-export class RewardService {}
+export class RewardService {
+  constructor(private readonly rewardProvider: RewardProvider) {}
+
+  async updateMintedState(userId: string, state: boolean) {
+    return await this.rewardProvider.updateMintedState(userId, state);
+  }
+
+  async incrementDailyCount(ctx: EUpdateTaskCount) {
+    return await this.rewardProvider.incrementDailyCount(ctx.userId);
+  }
+}
