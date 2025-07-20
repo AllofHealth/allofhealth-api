68802a16ad45827dd1785a66f3e9e8a4ddc84c71
3illBaby <thrillanon@outlook.com>
2025-07-20 17:49:18 +0100

    Add event listener for task completion rewards

--- src/modules/reward/service/reward.service.ts
+++ src/modules/reward/service/reward.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { RewardProvider } from '../provider/reward.provider';
 import { EUpdateTaskCount } from '@/shared/dtos/event.dto';
+import { OnEvent } from '@nestjs/event-emitter';
+import { SharedEvents } from '@/shared/events/shared.events';
 
 @Injectable()
 export class RewardService {
@@ -10,6 +12,7 @@ export class RewardService {
     return await this.rewardProvider.updateMintedState(userId, state);
   }
 
+  @OnEvent(SharedEvents.TASK_COMPLETED)
   async incrementDailyCount(ctx: EUpdateTaskCount) {
     return await this.rewardProvider.incrementDailyCount(ctx.userId);
   }
