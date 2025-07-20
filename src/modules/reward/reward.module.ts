3fe5ccbb491f80d1831982a80f753edef30a9bd5
3illBaby <thrillanon@outlook.com>
2025-07-20 17:39:00 +0100

    Add MintQueueModule to reward module imports

--- src/modules/reward/reward.module.ts
+++ src/modules/reward/reward.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { RewardProvider } from './provider/reward.provider';
 import { RewardService } from './service/reward.service';
 import { ErrorHandler } from '@/shared/error-handler/error.handler';
+import { MintQueueModule } from '@/shared/queues/mint/mint-queue.module';
 
 @Module({
+  imports: [MintQueueModule],
   providers: [RewardProvider, RewardService, ErrorHandler],
   exports: [RewardService],
 })
