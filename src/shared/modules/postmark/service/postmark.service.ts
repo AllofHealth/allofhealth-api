720c400876cb6bd560ed07f8c239cbb4744e2ff7
3illBaby <thrillanon@outlook.com>
2025-07-21 07:46:57 +0100

    feat(postmark.service.st): consumed provider

--- src/shared/modules/postmark/service/postmark.service.ts
+++ src/shared/modules/postmark/service/postmark.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PostmarkProvider } from '../provider/postmark.provider';
+import { ISendEmail } from '../interface/postmark.interface';
 
 @Injectable()
-export class PostmarkService {}
+export class PostmarkService {
+  constructor(private readonly postmarkProvider: PostmarkProvider) {}
+
+  async sendEmail(ctx: ISendEmail) {
+    return await this.postmarkProvider.sendEmail(ctx);
+  }
+}
