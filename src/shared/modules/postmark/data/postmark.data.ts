c928e89deafee16237e9b9e753779ba2c71a9a1e
3illBaby <thrillanon@outlook.com>
2025-07-21 07:45:33 +0100

    chore(postmark.data.ts): added data

---
+++ src/shared/modules/postmark/data/postmark.data.ts
@@ -0,0 +1,12 @@
+export enum POSTMARK_ERROR_MESSAGES {
+  ERROR_SENDING_EMAIL = 'Error sending email',
+}
+
+export enum EMAIL_CONFIG {
+  FROM = 'support@allofhealth.africa',
+  SUBJECT = 'Verification',
+}
+
+export enum POSTMAN_SUCCESS_MESSAGES {
+  SUCCESS_SENDING_EMAIL = 'Email sent successfully',
+}
