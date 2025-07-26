967ae1b5884156aa2bc3c38ca68f0a7af2f5e4b0
3illBaby <thrillanon@outlook.com>
2025-07-26 20:07:18 +0100

    Remove unused CronExpression import

--- src/modules/asset/tasks/file-cleanup.service.ts
+++ src/modules/asset/tasks/file-cleanup.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { Cron, CronExpression } from '@nestjs/schedule';
+import { Cron } from '@nestjs/schedule';
 import * as fs from 'fs';
 import * as path from 'path';
 
