b304136b66ed80aef0e6f8095272d2f8f3bdaa86
3illBaby <thrillanon@outlook.com>
2025-08-01 15:58:18 +0100

    Add fetchTokenBalance method to WalletService

--- src/modules/wallet/service/wallet.service.ts
+++ src/modules/wallet/service/wallet.service.ts
@@ -4,8 +4,11 @@ import { WalletProvider } from '../provider/wallet.provider';
 @Injectable()
 export class WalletService {
   constructor(private readonly walletProvider: WalletProvider) {}
-
   async fetchUserWallet(userId: string) {
     return await this.walletProvider.fetchUserWallet(userId);
+  }
+
+  async fetchTokenBalance(userId: string) {
+    return await this.walletProvider.fetchUserTokenBalance(userId);
   }
 }
