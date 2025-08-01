b4ff16af0d323b7a78c1c7049be27ac32060acd5
3illBaby <thrillanon@outlook.com>
2025-08-01 15:57:57 +0100

    Add controllers and module dependencies to RewardModule

--- src/modules/reward/reward.module.ts
+++ src/modules/reward/reward.module.ts
@@ -1,12 +1,21 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { RewardProvider } from './provider/reward.provider';
 import { RewardService } from './service/reward.service';
 import { ErrorHandler } from '@/shared/error-handler/error.handler';
 import { MintQueueModule } from '@/shared/queues/mint/mint-queue.module';
 import { RewardDistributionService } from './tasks/reward-distrubtion.task';
+import { ContractModule } from '../contract/contract.module';
+import { RewardController } from './controller/reward.controller';
+import { UserModule } from '../user/user.module';
+import { TokenModule } from '../token/token.module';
 
 @Module({
-  imports: [MintQueueModule],
+  imports: [
+    MintQueueModule,
+    forwardRef(() => ContractModule),
+    forwardRef(() => UserModule),
+    forwardRef(() => TokenModule),
+  ],
   providers: [
     RewardProvider,
     RewardService,
@@ -14,5 +23,6 @@ import { RewardDistributionService } from './tasks/reward-distrubtion.task';
     RewardDistributionService,
   ],
   exports: [RewardService],
+  controllers: [RewardController],
 })
 export class RewardModule {}
