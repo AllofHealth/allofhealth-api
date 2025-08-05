e9f85712b6982fa35e35d372b1673b0be1792b9f
3illBaby <thrillanon@outlook.com>
2025-08-05 14:25:41 +0100

    feat(resend.service.ts): consumed provider

--- src/shared/modules/resend/service/resend.service.ts
+++ src/shared/modules/resend/service/resend.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ResendProvider } from '../provider/resend.provider';
+import { ISendEmail } from '../interface/resend.interface';
 
 @Injectable()
-export class ResendService {}
+export class ResendService {
+  constructor(private readonly resendProvider: ResendProvider) {}
+
+  async sendEmail(ctx: ISendEmail) {
+    return await this.resendProvider.sendEmail(ctx);
+  }
+}
