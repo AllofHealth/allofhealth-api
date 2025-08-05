950af28a072b362b043dcdb1a24a997ab6cce407
3illBaby <thrillanon@outlook.com>
2025-08-05 14:10:21 +0100

    chore(resend.data.ts): added resend data

--- src/shared/modules/resend/resend.module.ts
+++ src/shared/modules/resend/resend.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ResendService } from './service/resend.service';
 import { ResendProvider } from './provider/resend.provider';
+import { ErrorHandler } from '@/shared/error-handler/error.handler';
 
 @Module({
-  providers: [ResendProvider, ResendService],
+  providers: [ResendProvider, ResendService, ErrorHandler],
   exports: [ResendService],
 })
 export class ResendModule {}
