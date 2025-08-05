c11e20b8c15ba36dc9a11c9b31021b88ee9fc632
3illBaby <thrillanon@outlook.com>
2025-08-05 14:24:53 +0100

    chore(resend.interface.ts): added interface

--- src/shared/modules/resend/interface/resend.interface.ts
+++ src/shared/modules/resend/interface/resend.interface.ts
@@ -1,1 +1,10 @@
-export interface Resend {}
+export type TEmailContext = 'WELCOME' | 'OTP' | 'APPROVAL';
+
+export interface ISendEmail {
+  to: string;
+  subject: string;
+  body: string;
+  from?: string;
+  useHtml?: boolean;
+  context?: TEmailContext;
+}
