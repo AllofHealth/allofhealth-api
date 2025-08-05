eed9a0e1eb897534a6449c0e413c676d3d91fe14
3illBaby <thrillanon@outlook.com>
2025-08-05 15:59:12 +0100

    Add email verification test to ResendService spec

--- src/shared/modules/resend/service/resend.service.spec.ts
+++ src/shared/modules/resend/service/resend.service.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ResendService } from './resend.service';
+import { ResendProvider } from '../provider/resend.provider';
+import { RESEND_API_KEY } from '@/shared/data/constants';
+import { ResendConfig } from '@/shared/config/resend/resend.config';
+import { ErrorHandler } from '@/shared/error-handler/error.handler';
 
 describe('ResendService', () => {
   let service: ResendService;
+  const mockConfig = {
+    RESEND_API_KEY,
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ResendService],
+      providers: [
+        ResendService,
+        ResendProvider,
+        ErrorHandler,
+        {
+          provide: ResendConfig,
+          useValue: mockConfig,
+        },
+      ],
     }).compile();
 
     service = module.get<ResendService>(ResendService);
@@ -14,5 +29,18 @@ describe('ResendService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('Emails', () => {
+    it('Should successfully send an email to outbound domain', async () => {
+      const response = await service.sendEmail({
+        to: 'preciousegbu@gmail.com',
+        body: '23456',
+        subject: 'OTP VERIFICATION',
+      });
+
+      console.log(response);
+      expect(response).toBeDefined();
+    }, 5000);
   });
 });
