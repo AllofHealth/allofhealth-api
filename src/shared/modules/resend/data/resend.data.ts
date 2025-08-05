fe9cdb3e9b3a403655e73eac9795b722b9ef9fbd
3illBaby <thrillanon@outlook.com>
2025-08-05 14:10:53 +0100

    chore(resend.data.ts): added constant data

---
+++ src/shared/modules/resend/data/resend.data.ts
@@ -0,0 +1,13 @@
+export enum RESEND_SUCCESS_MESSAGE {
+  EMAIL_SENT = 'Email sent successfully',
+}
+
+export enum RESEND_ERROR_MESSAGE {
+  EMAIL_NOT_SENT = 'Email not sent',
+  ERROR_SENDING_EMAIL = 'Error sending email',
+}
+
+export enum RESEND_EMAIL_CONFIG {
+  FROM = 'support@allofhealth.africa',
+  SUBJECT = 'Verification',
+}
