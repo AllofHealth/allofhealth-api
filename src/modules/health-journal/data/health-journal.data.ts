b3f33563480e1bbe5ce3076b25b682ef9e496737
3illBaby <thrillanon@outlook.com>
2025-08-08 22:47:01 +0100

    chore(health-journal.data.ts): added month to index map

--- src/modules/health-journal/data/health-journal.data.ts
+++ src/modules/health-journal/data/health-journal.data.ts
@@ -2,10 +2,19 @@ export enum HealthJournalErrorMessages {
   ERROR_ADDING_ENTRY = 'Error adding entry',
   ERROR_FETCHING_JOURNAL = 'Error fetching journal',
   ERROR_FETCHING_MONTHLY_JOURNAL = 'Error fetching monthly journal',
+  ERROR_CALCULATING_MONTHLY_AVERAGE_MOOD_SCORE = 'Error calculating monthly average mood score',
 }
 
 export enum HealthJournalSuccessMessages {
   SUCCESS_ADDING_ENTRY = 'Entry added successfully',
   SUCCESS_FETCHING_JOURNAL = 'Journal fetched successfully',
   SUCCESS_FETCHING_MONTHLY_JOURNAL = 'Monthly journal fetched successfully',
+}
+
+export enum MOOD_INDEX {
+  BAD = 0,
+  LOW = 1,
+  NEUTRAL = 2,
+  GOOD = 3,
+  GREAT = 4,
 }
