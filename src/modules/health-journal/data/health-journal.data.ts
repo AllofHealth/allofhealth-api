9fcd867f408f17ddad2a06bf5f74d5b100c205f2
3illBaby <thrillanon@outlook.com>
2025-08-08 21:40:02 +0100

    chore(health-journal.data.ts): added journal constants

--- src/modules/health-journal/data/health-journal.data.ts
+++ src/modules/health-journal/data/health-journal.data.ts
@@ -1,9 +1,11 @@
 export enum HealthJournalErrorMessages {
   ERROR_ADDING_ENTRY = 'Error adding entry',
   ERROR_FETCHING_JOURNAL = 'Error fetching journal',
+  ERROR_FETCHING_MONTHLY_JOURNAL = 'Error fetching monthly journal',
 }
 
 export enum HealthJournalSuccessMessages {
   SUCCESS_ADDING_ENTRY = 'Entry added successfully',
   SUCCESS_FETCHING_JOURNAL = 'Journal fetched successfully',
+  SUCCESS_FETCHING_MONTHLY_JOURNAL = 'Monthly journal fetched successfully',
 }
