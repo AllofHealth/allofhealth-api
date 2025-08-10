96c46d15a87211d5630de4022afcce03d78cf0fb
3illBaby <thrillanon@outlook.com>
2025-08-11 00:06:12 +0100

    Add ContractModule and RecordsModule to queue dependencies

--- src/shared/queues/records/records-queue.module.ts
+++ src/shared/queues/records/records-queue.module.ts
@@ -1,13 +1,17 @@
 import { BullModule } from '@nestjs/bull';
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { CreateRecordQueue } from './records.queue';
 import { CreateRecordProcessor } from '@/shared/processors/records.processor';
+import { ContractModule } from '@/modules/contract/contract.module';
+import { RecordsModule } from '@/modules/records/records.module';
 
 @Module({
   imports: [
     BullModule.registerQueue({
       name: 'create-record-queue',
     }),
+    forwardRef(() => RecordsModule),
+    ContractModule,
   ],
   providers: [CreateRecordQueue, CreateRecordProcessor],
   exports: [CreateRecordQueue],
