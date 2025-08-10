7469e5f98b5bc10aa17bf6c83df0f5585c1833d2
3illBaby <thrillanon@outlook.com>
2025-08-10 23:05:56 +0100

    chore(records-queue.module.ts): imported processor

--- src/shared/queues/records/records-queue.module.ts
+++ src/shared/queues/records/records-queue.module.ts
@@ -1,13 +1,15 @@
 import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
+import { CreateRecordQueue } from './records.queue';
+import { CreateRecordProcessor } from '@/shared/processors/records.processor';
 
 @Module({
   imports: [
     BullModule.registerQueue({
-      name: 'handle-upload-record-queue',
+      name: 'create-record-queue',
     }),
   ],
-  providers: [],
-  exports: [],
+  providers: [CreateRecordQueue, CreateRecordProcessor],
+  exports: [CreateRecordQueue],
 })
 export class RecordsQueueModule {}
