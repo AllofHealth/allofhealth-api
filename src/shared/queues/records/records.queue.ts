047d28029e2e2e9f915ea756c7bed4be52d166f6
3illBaby <thrillanon@outlook.com>
2025-08-17 18:52:41 +0100

    chore(records.queue.ts): added logs

--- src/shared/queues/records/records.queue.ts
+++ src/shared/queues/records/records.queue.ts
@@ -1,3 +1,4 @@
+import { MyLoggerService } from '@/modules/my-logger/service/my-logger.service';
 import { EAddMedicalRecordToContract } from '@/shared/dtos/event.dto';
 import { InjectQueue } from '@nestjs/bull';
 import { Injectable } from '@nestjs/common';
@@ -5,12 +6,14 @@ import type { Queue } from 'bull';
 
 @Injectable()
 export class CreateRecordQueue {
+  private readonly logger = new MyLoggerService(CreateRecordQueue.name);
   constructor(
     @InjectQueue('create-record-queue')
     private readonly createRecordQueue: Queue,
   ) {}
 
   async createRecordJob(data: EAddMedicalRecordToContract) {
+    this.logger.debug('Creating record job');
     await this.createRecordQueue.add('record-to-contract', data, {
       attempts: 3,
       backoff: {
