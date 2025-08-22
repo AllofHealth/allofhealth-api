0ff123446135af2054b8c5e8f758f4a80354296a
3illBaby <thrillanon@outlook.com>
2025-08-22 22:40:26 +0100

    chore(admin.data.ts): added constant data

--- src/modules/admin/data/admin.data.ts
+++ src/modules/admin/data/admin.data.ts
@@ -11,6 +11,8 @@ export enum ADMIN_ERROR_MESSAGES {
   INVALID_ADMIN_PASSWORD = 'Invalid admin password',
   ERROR_VERIFYING_PRACTITIONER = 'Error verifying practitioner',
   ERROR_FETCHING_ACTIVE_USERS = 'Error fetching active users',
+  ERROR_VALIDATING_SUSPENSION_STATUS = 'Error validating suspension status',
+  ERROR_SUSPENDING_USER = 'Error suspending user',
 }
 
 export enum ADMIN_SUCCESS_MESSAGES {
@@ -21,8 +23,15 @@ export enum ADMIN_SUCCESS_MESSAGES {
   SUCCESS_LOGGING_IN_AS_ADMIN = 'Admin logged in successfully',
   PRACTITIONER_VERIFIED = 'Practitioner verified successfully',
   SUCCESS_DELETING_ADMIN = 'Admin deleted successfully',
+  USER_ALREADY_SUSPENDED = 'User is already suspended',
+  USER_SUSPENDED_SUCCESSFULLY = 'User suspended successfully',
 }
 
 export enum ACTIVITY_THRESHOLD {
   ACTIVE = 1000 * 60 * 60 * 24 * 7, //  7 Days
+}
+
+export enum SUSPENSION_REASON {
+  DEFAULT = 'User violated all of health terms and conditions',
+  ACTIVE_SUSPENSION = 'User has been suspended for violating all of health terms and conditions',
 }
