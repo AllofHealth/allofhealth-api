ddd08e1b0ea01162972db3077ad76a4d9af8b60e
3illBaby <thrillanon@outlook.com>
2025-08-26 14:27:19 +0100

    chore(doctor.data.ts): added doctor constant data

--- src/modules/doctor/data/doctor.data.ts
+++ src/modules/doctor/data/doctor.data.ts
@@ -3,9 +3,11 @@ export enum DOCTOR_ERROR_MESSGAES {
   DOCTOR_NOT_FOUND = 'Doctor not found',
   DOCTOR_ALREADY_EXISTS = 'Doctor already exists',
   ERROR_FETCHING_ALL_DOCTORS = 'Error fetching all doctors',
+  ERROR_UPDATING_RECORDS_REVIEWED_COUNT = 'Error updating records reviewed count',
 }
 
 export enum DOCTOR_SUCCESS_MESSAGES {
   DOCTOR_CREATED = 'Doctor created successfully',
   DOCTOR_FETCHED = 'Doctor fetched successfully',
+  RECORD_REVIEW_COUNT_UPDATED = 'Record review count updated successfully',
 }
