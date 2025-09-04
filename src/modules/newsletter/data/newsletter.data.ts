cbfae77c0b17ac5b3f23710cdb7be402d8b1ed65
3illBaby <thrillanon@outlook.com>
2025-09-04 22:32:49 +0100

    chore(newsletter.module.ts): init newsletter module

---
+++ src/modules/newsletter/data/newsletter.data.ts
@@ -0,0 +1,9 @@
+export enum NEWS_LETTER_ERROR_MESSAGES {
+  ERROR_SUBSCRIBING = 'Error subscribing to newsletter',
+  ERROR_FETCHING_SUBSCRIBERS = 'Error fetching subscribers',
+}
+
+export enum NEWS_LETTER_SUCCESS_MESSGAES {
+  SUCCESS_SUBSCRIBING = 'Successfully subscribed to newsletter',
+  SUCCESS_FETCHING_SUBSCRIBERS = 'Successfully fetched subscribers',
+}
