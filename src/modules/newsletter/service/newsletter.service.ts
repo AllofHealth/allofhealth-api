5603167acc817614065fcaa7d45da23e0bc52ec0
3illBaby <thrillanon@outlook.com>
2025-09-04 22:44:22 +0100

    feat(newsletter.service.ts): exposed function to service layer

--- src/modules/newsletter/service/newsletter.service.ts
+++ src/modules/newsletter/service/newsletter.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { NewsletterProvider } from '../provider/newsletter.provider';
+import {
+  ICreateContact,
+  IFetchAllContacts,
+} from '@/shared/modules/brevo/interface/brevo.interface';
 
 @Injectable()
-export class NewsletterService {}
+export class NewsletterService {
+  constructor(private readonly newsletterProvider: NewsletterProvider) {}
+
+  async subscribe(ctx: ICreateContact) {
+    return await this.newsletterProvider.subscribe(ctx);
+  }
+
+  async fetchAllSubscribers(ctx: IFetchAllContacts) {
+    return await this.newsletterProvider.fetchAllSubscribers(ctx);
+  }
+}
