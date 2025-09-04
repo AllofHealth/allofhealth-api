d5584c749afafeafb9ab5a3d68eeb68572c98bf8
3illBaby <thrillanon@outlook.com>
2025-09-04 16:52:51 +0100

    test(brevo.provider.spec.ts): added brevo config

--- src/shared/modules/brevo/provider/brevo.provider.spec.ts
+++ src/shared/modules/brevo/provider/brevo.provider.spec.ts
@@ -1,12 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BrevoProvider } from './brevo.provider';
+import { BREVO_API_KEY } from '@/shared/data/constants';
+import { BrevoConfig } from '@/shared/config/brevo/brevo.config';
 
 describe('BrevoProvider', () => {
   let provider: BrevoProvider;
+  const mockBrevoConfig = {
+    BREVO_API_KEY,
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BrevoProvider],
+      providers: [
+        BrevoProvider,
+        {
+          provide: BrevoConfig,
+          useValue: mockBrevoConfig,
+        },
+      ],
     }).compile();
 
     provider = module.get<BrevoProvider>(BrevoProvider);
