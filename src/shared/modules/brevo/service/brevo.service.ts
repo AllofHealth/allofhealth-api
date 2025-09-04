90615a12f3335fb325e460ff1f8108d25b0b4834
3illBaby <thrillanon@outlook.com>
2025-09-04 22:27:16 +0100

    feat(brevo.service.ts): added func to service layer

--- src/shared/modules/brevo/service/brevo.service.ts
+++ src/shared/modules/brevo/service/brevo.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { BrevoProvider } from '../provider/brevo.provider';
+import {
+  ICreateContact,
+  IFetchAllContacts,
+} from '../interface/brevo.interface';
 
 @Injectable()
-export class BrevoService {}
+export class BrevoService {
+  constructor(private readonly brevoProvider: BrevoProvider) {}
+
+  async createContact(ctx: ICreateContact) {
+    return await this.brevoProvider.createContact(ctx);
+  }
+
+  async fetchAllContacts(ctx: IFetchAllContacts) {
+    return await this.brevoProvider.getAllContacts(ctx);
+  }
+}
