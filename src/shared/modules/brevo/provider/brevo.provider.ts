622a25417e50cb6d8166621c0b37d146b27ceca7
3illBaby <thrillanon@outlook.com>
2025-09-04 16:53:18 +0100

    feat(brevo.provider.ts): impl func to get brevo options

--- src/shared/modules/brevo/provider/brevo.provider.ts
+++ src/shared/modules/brevo/provider/brevo.provider.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { BrevoConfig } from '@/shared/config/brevo/brevo.config';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { IGetBrevoOptions } from '../interface/brevo.interface';
 
 @Injectable()
-export class BrevoProvider {}
+export class BrevoProvider {
+  constructor(private readonly brevoConfig: BrevoConfig) {}
+
+  private getBrevoOptions(ctx: IGetBrevoOptions) {
+    const { method, body } = ctx;
+    if (method === 'POST' && !body) {
+      throw new BadRequestException('Body is required for POST method');
+    }
+    switch (method) {
+      case 'POST':
+        return {
+          method: method,
+          headers: {
+            accept: 'application/json',
+            'content-Type': 'application/json',
+            'api-key': this.brevoConfig.BREVO_API_KEY,
+          },
+          body: JSON.stringify({
+            ...body,
+            updateEnabled: false,
+          }),
+        };
+      case 'GET':
+        return {
+          method: method,
+          headers: {
+            accept: 'application/json',
+            'api-key': this.brevoConfig.BREVO_API_KEY,
+          },
+        };
+    }
+  }
+
+  async createContact() {}
+}
