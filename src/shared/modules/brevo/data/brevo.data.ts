e793d7da533188a18bb8a86a76678b6d14b6772a
3illBaby <thrillanon@outlook.com>
2025-09-04 22:21:06 +0100

    chore(brevo.data.ts): added constant

--- src/shared/modules/brevo/data/brevo.data.ts
+++ src/shared/modules/brevo/data/brevo.data.ts
@@ -1,9 +1,11 @@
 export enum BREVO_ERROR_MESSAGES {
   ERROR_CREATING_CONTACT = 'Error creating contact',
+  ERROR_FETCHING_CONTACTS = 'Error fetching contacts',
 }
 
 export enum BREVO_SUCCESS_MESSAGES {
   SUCCESS_CREATING_CONTACT = 'Contact created successfully',
+  SUCCESS_FETCHING_CONTACTS = 'Contacts fetched successfully',
 }
 
 export enum CONTACT_PATH {
