adcc6d0f73f33be91e4e65ee61d086d265810733
3illBaby <thrillanon@outlook.com>
2025-09-04 07:49:55 +0100

    chore(ipfs.data.ts): added constants

--- src/modules/ipfs/data/ipfs.data.ts
+++ src/modules/ipfs/data/ipfs.data.ts
@@ -2,9 +2,11 @@ export enum IPFS_ERROR_MESSAGES {
   ERROR_UPLOADING_RECORD = 'Error uploading record to IPFS',
   ERROR_FETCHING_RECORD = 'Error fetching record from IPFS',
   ERROR_DELETING_RECORD = 'Error deleting record from IPFS',
+  ERROR_FINDING_PATH = 'Error finding path from CID',
 }
 
 export enum IPFS_SUCCESS_MESSAGES {
   SUCCESS_UPLOADING_RECORD = 'Record uploaded successfully to IPFS',
   SUCCESS_DELETING_RECORD = 'Record deleted successfully from IPFS',
+  SUCCESS_FINDING_PATH = 'Path found successfully from CID',
 }
