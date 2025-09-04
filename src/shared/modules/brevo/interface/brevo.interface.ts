41da2d5a2cea91f16713b9b45e56823052756e5b
3illBaby <thrillanon@outlook.com>
2025-09-04 16:52:14 +0100

    chore(brevo.interface.ts): added interface

--- src/shared/modules/brevo/interface/brevo.interface.ts
+++ src/shared/modules/brevo/interface/brevo.interface.ts
@@ -1,1 +1,10 @@
-export interface Brevo {}
+export type TRequest = 'POST' | 'GET' | 'PUT' | 'DELETE';
+
+export interface IGetBrevoOptions {
+  method: TRequest;
+  body?: Object;
+}
+
+export interface ICreateContact {
+  emailAddress: string;
+}
