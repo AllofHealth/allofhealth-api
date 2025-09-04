8f4caaf008f54aaff520fe6c5fec5dd63bb85aae
3illBaby <thrillanon@outlook.com>
2025-09-04 17:06:46 +0100

    feat(brevo.provider.ts): impl func to create contact

--- src/shared/modules/brevo/provider/brevo.provider.ts
+++ src/shared/modules/brevo/provider/brevo.provider.ts
@@ -1,6 +1,17 @@
 import { BrevoConfig } from '@/shared/config/brevo/brevo.config';
-import { BadRequestException, Injectable } from '@nestjs/common';
-import { IGetBrevoOptions } from '../interface/brevo.interface';
+import {
+  BadRequestException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
+import { ICreateContact, IGetBrevoOptions } from '../interface/brevo.interface';
+import { BrevoError } from '../error/brevo.error';
+import {
+  BREVO_ERROR_MESSAGES as BEM,
+  BREVO_SUCCESS_MESSAGES as BSM,
+  CONTACT_PATH,
+} from '../data/brevo.data';
 
 @Injectable()
 export class BrevoProvider {
@@ -11,30 +22,62 @@ export class BrevoProvider {
     if (method === 'POST' && !body) {
       throw new BadRequestException('Body is required for POST method');
     }
-    switch (method) {
-      case 'POST':
-        return {
-          method: method,
-          headers: {
-            accept: 'application/json',
-            'content-Type': 'application/json',
-            'api-key': this.brevoConfig.BREVO_API_KEY,
-          },
-          body: JSON.stringify({
-            ...body,
-            updateEnabled: false,
-          }),
-        };
-      case 'GET':
-        return {
-          method: method,
-          headers: {
-            accept: 'application/json',
-            'api-key': this.brevoConfig.BREVO_API_KEY,
-          },
-        };
+
+    if (method === 'POST') {
+      return {
+        method: method,
+        headers: {
+          accept: 'application/json',
+          'content-Type': 'application/json',
+          'api-key': this.brevoConfig.BREVO_API_KEY,
+        },
+        body: JSON.stringify({
+          ...body,
+          updateEnabled: false,
+        }),
+      };
     }
+
+    if (method === 'GET') {
+      return {
+        method: method,
+        headers: {
+          accept: 'application/json',
+          'api-key': this.brevoConfig.BREVO_API_KEY,
+        },
+      };
+    }
+    throw new HttpException(
+      new BrevoError('Invalid Method'),
+      HttpStatus.BAD_REQUEST,
+    );
   }
 
-  async createContact() {}
+  async createContact(ctx: ICreateContact) {
+    try {
+      const options = this.getBrevoOptions({
+        method: 'POST',
+        body: {
+          email: ctx.emailAddress,
+        },
+      });
+
+      const response = await fetch(CONTACT_PATH.CONTACTS, options as Object);
+      const data = await response.json();
+      if (response.ok) {
+        return data;
+      }
+      throw new HttpException(
+        new BrevoError(BEM.ERROR_CREATING_CONTACT),
+        HttpStatus.BAD_REQUEST,
+        { cause: data },
+      );
+    } catch (e) {
+      throw new HttpException(
+        new BrevoError(BEM.ERROR_CREATING_CONTACT),
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        { cause: e },
+      );
+    }
+  }
 }
