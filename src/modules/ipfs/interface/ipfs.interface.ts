1e0a06b7cf549858890a7128bab9dd04fd8cb472
3illBaby <thrillanon@outlook.com>
2025-09-04 07:50:15 +0100

    chore(ipfs.interface.ts): added interface

--- src/modules/ipfs/interface/ipfs.interface.ts
+++ src/modules/ipfs/interface/ipfs.interface.ts
@@ -13,6 +13,9 @@ export interface IMedicalRecord {
   attachments?: Express.Multer.File[];
 }
 
+/**
+ * @dev Medications prescribed is added twice intentionally. previous records might still have that field in typo. but newer records should have the issue fixed
+ */
 export interface IpfsRecord {
   userId: string;
   title: string;
@@ -20,11 +23,17 @@ export interface IpfsRecord {
   diagnosis: string[];
   labResults?: string[];
   medicationsPrscribed?: string[];
+  medicationsPrescribed?: string[];
   attachments?: string[];
   uploadedAt: string;
 }
 
 export interface IDeleteRecordFromIpfs {
+  userId: string;
+  cid: string;
+}
+
+export interface IFindPathFromCid {
   userId: string;
   cid: string;
 }
