7a30b491606d011914b728d67994d96641f67120
3illBaby <thrillanon@outlook.com>
2025-09-05 11:23:10 +0100

    chore(resend.interface.ts): extended intyerface

--- src/shared/modules/resend/interface/resend.interface.ts
+++ src/shared/modules/resend/interface/resend.interface.ts
@@ -1,10 +1,28 @@
 export type TEmailContext = 'WELCOME' | 'OTP' | 'APPROVAL';
 
 export interface ISendEmail {
+  name?: string;
   to: string;
   subject: string;
   body: string;
   from?: string;
   useHtml?: boolean;
   context?: TEmailContext;
+}
+
+export interface IHandleOnboarding {
+  from?: string;
+  to?: string;
+  subject?: string;
+  name: string;
+  nextStepUrl?: string;
+}
+
+export interface IHandleOtp {
+  name: string;
+  to: string;
+  from?: string;
+  subject?: string;
+  verifyUrl?: string;
+  code: string;
 }
