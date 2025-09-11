bdc5118f03c9de45f6d5f7267ed092819f3a9151
3illBaby <thrillanon@outlook.com>
2025-09-11 10:20:46 +0100

    Improve error handling in reward provider methods

--- src/modules/reward/provider/reward.provider.ts
+++ src/modules/reward/provider/reward.provider.ts
@@ -204,7 +204,7 @@ export class RewardProvider {
         message: RSM.REWARD_UPDATED,
       });
     } catch (e) {
-      this.handler.handleError(e, REM.ERROR_UPDATING_REWARD);
+      this.handler.handleError(e, e.message || REM.ERROR_UPDATING_REWARD);
     }
   }
 
@@ -217,7 +217,7 @@ export class RewardProvider {
         })
         .where(eq(schema.dailyReward.userId, userId));
     } catch (e) {
-      this.handler.handleError(e, REM.ERROR_UPDATING_REWARD);
+      this.handler.handleError(e, e.message || REM.ERROR_UPDATING_REWARD);
     }
   }
 
@@ -229,10 +229,7 @@ export class RewardProvider {
       }
       const tokenBalance = balance.data;
       if (!tokenBalance) {
-        return this.handler.handleReturn({
-          status: HttpStatus.OK,
-          message: REM.TOKEN_BALANCE_NOT_FOUND,
-        });
+        throw new NotFoundException(REM.TOKEN_BALANCE_NOT_FOUND);
       }
 
       const [claimedBalance, pendingRewards] = await Promise.all([
@@ -260,7 +257,10 @@ export class RewardProvider {
         },
       });
     } catch (e) {
-      this.handler.handleError(e, REM.ERROR_FETCHING_REWARD_METIRCS);
+      this.handler.handleError(
+        e,
+        e.message || REM.ERROR_FETCHING_REWARD_METIRCS,
+      );
     }
   }
 }
