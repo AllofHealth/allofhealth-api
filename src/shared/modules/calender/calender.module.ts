1714a0b35ca22c6ec22c3fb3c54530e8d8bcd086
3illBaby <thrillanon@outlook.com>
2025-10-24 17:38:54 +0100

    chore(calendar.module.ts): injected calcom module

--- src/shared/modules/calender/calender.module.ts
+++ src/shared/modules/calender/calender.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { CalcomService } from './service/calcom.service';
 import { DoxyService } from './service/doxy.service';
 import { CalendarIntegrationService } from './service/calendar-integration.service';
+import { CalComModule } from '../cal.com/cal.com.module';
 
 @Module({
-  providers: [CalcomService, DoxyService, CalendarIntegrationService],
-  exports: [CalcomService, DoxyService, CalendarIntegrationService],
+  imports: [CalComModule],
+  providers: [DoxyService, CalendarIntegrationService],
+  exports: [DoxyService, CalendarIntegrationService],
 })
 export class CalendarModule {}
