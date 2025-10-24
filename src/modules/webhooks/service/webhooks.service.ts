a67a1843ef58c9340a163429efa7d76ce3f250d5
3illBaby <thrillanon@outlook.com>
2025-10-24 17:26:49 +0100

    chore(webhooks.service.ts): init webhook service

--- src/modules/webhooks/service/webhooks.service.ts
+++ src/modules/webhooks/service/webhooks.service.ts
@@ -1,4 +1,39 @@
+import { CalConfig } from '@/shared/config/cal.com/cal.config';
+import { DoxyConfig } from '@/shared/config/doxy/doxy.config';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class WebhooksService {}
+export class WebhooksService {
+  constructor(
+    private readonly calConfig: CalConfig,
+    private readonly doxyConfig: DoxyConfig,
+  ) {}
+
+  verifyCalWebhookSignature(payload: string, signature: string): boolean {
+    const crypto = require('crypto');
+    const hmac = crypto.createHmac(
+      'sha256',
+      this.calConfig.CALCOM_WEBHOOK_SECRET,
+    );
+    const digest = hmac.update(payload).digest('hex');
+
+    return digest === signature;
+  }
+
+  verifyDoxyWebhookSignature(payload: string, signature: string): boolean {
+    const crypto = require('crypto');
+    const hmac = crypto.createHmac(
+      'sha256',
+      this.doxyConfig.DOXY_WEBHOOK_SECRET,
+    );
+    const digest = hmac.update(payload).digest('hex');
+
+    return digest === signature;
+  }
+
+  async procesCalEvents(req: Request) {}
+
+  async processDoxyEvents(req: Request) {}
+
+  async processFlutterEvents(req: Request) {}
+}
