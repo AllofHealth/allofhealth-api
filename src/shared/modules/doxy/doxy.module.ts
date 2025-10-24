8d10fb3f02f3e64754966c8fc43057370c71fb51
3illBaby <thrillanon@outlook.com>
2025-10-24 18:25:27 +0100

    chore(doxy.module.ts): init doxy module

--- src/shared/modules/doxy/doxy.module.ts
+++ src/shared/modules/doxy/doxy.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { Doxy } from './doxy';
+import { DoxyService } from './doxy.service';
 
-@Module({})
+@Module({
+  providers: [Doxy, DoxyService]
+})
 export class DoxyModule {}
