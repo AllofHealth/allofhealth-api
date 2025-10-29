df4469a0f79bd464ebddc1b9a30965a6d32815a1
3illBaby <thrillanon@outlook.com>
2025-10-29 16:49:15 +0100

    chore(booking.data.ts): added static data for booking

---
+++ src/modules/booking/data/booking.data.ts
@@ -0,0 +1,11 @@
+export enum BOOKING_ERROR_MESSAGES {
+  ERROR_CREATE_BOOKING = 'Error creating booking',
+  BOOKING_NOT_FOUND = 'Booking not found',
+  ERROR_FINDING_BOOKING = 'Error finding booking',
+  ERROR_UPDATING_BOOKING_STATUS = 'Error updating booking status',
+}
+export enum BOOKING_SUCCESS_MESSAGES {
+  SUCCESS_CREATE_BOOKING = 'Booking created successfully',
+  SUCCESS_FIND_BOOKING = 'Booking found successfully',
+  SUCCESS_UPDATE_BOOKING_STATUS = 'Booking status updated successfully',
+}
