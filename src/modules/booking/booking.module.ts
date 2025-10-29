84f230abb41e6b5993b5d7ac861aa84ed1ceacfe
3illBaby <thrillanon@outlook.com>
2025-10-29 16:58:00 +0100

    chore(booking.module.ts): added handler provider

--- src/modules/booking/booking.module.ts
+++ src/modules/booking/booking.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { BookingService } from './service/booking.service';
 import { BookingController } from './controller/booking.controller';
 import { BookingProvider } from './provider/booking.provider';
+import { ErrorHandler } from '@/shared/error-handler/error.handler';
 
 @Module({
-  providers: [BookingProvider, BookingService],
+  providers: [BookingProvider, BookingService, ErrorHandler],
   controllers: [BookingController],
   exports: [BookingService],
 })
