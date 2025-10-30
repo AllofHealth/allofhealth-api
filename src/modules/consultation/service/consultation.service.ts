3318e17e89766326cb9e64bea22e22ffe08d1254
3illBaby <thrillanon@outlook.com>
2025-10-30 20:04:08 +0100

    fix(consultation.service.ts): fixed id type

--- src/modules/consultation/service/consultation.service.ts
+++ src/modules/consultation/service/consultation.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { ConsultationProvider } from '../provider/consultation.provider';
+import {
+  ICreateConsultationType,
+  IGetDoctorConsultationTypes,
+  IUpdateConsultationType,
+} from '../interface/consultation.interface';
 
 @Injectable()
-export class ConsultationService {}
+export class ConsultationService {
+  constructor(private readonly consultationProvider: ConsultationProvider) {}
+
+  async createConsultationType(ctx: ICreateConsultationType) {
+    return await this.consultationProvider.createConsultationType(ctx);
+  }
+
+  async getDoctorConsultationTypes(ctx: IGetDoctorConsultationTypes) {
+    return await this.consultationProvider.getDoctorConsultationTypes(ctx);
+  }
+
+  async updateConsultationType(ctx: IUpdateConsultationType) {
+    return await this.consultationProvider.updateConsultationType(ctx);
+  }
+
+  async findById(id: string) {
+    return await this.consultationProvider.findById(id);
+  }
+
+  async findCalcomEventId(id: number) {
+    return await this.consultationProvider.findByCalcomEventTypeId(id);
+  }
+
+  async deleteConsultationType(id: string) {
+    return await this.consultationProvider.deleteConsultationType(id);
+  }
+}
