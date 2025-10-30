19646285b77d247be8faa641bd2f4b88ad65d327
3illBaby <thrillanon@outlook.com>
2025-10-30 20:02:09 +0100

    chore(consultation.module.ts): imported handler, exported service

--- src/modules/consultation/consultation.module.ts
+++ src/modules/consultation/consultation.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConsultationProvider } from './provider/consultation.provider';
 import { ConsultationService } from './service/consultation.service';
 import { ConsultationController } from './controller/consultation.controller';
+import { ErrorHandler } from '@/shared/error-handler/error.handler';
 
 @Module({
-  providers: [ConsultationService, ConsultationProvider],
+  providers: [ConsultationService, ConsultationProvider, ErrorHandler],
   controllers: [ConsultationController],
   exports: [ConsultationService],
 })
