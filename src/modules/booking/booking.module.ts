ee8e8de3357d86a515cfc29e0ada9f3b49b6cdb3
3illBaby <thrillanon@outlook.com>
2025-11-04 21:00:29 +0100

    chore(booking.module.ts): import user module

--- src/modules/booking/booking.module.ts
+++ src/modules/booking/booking.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { BookingService } from './service/booking.service';
 import { BookingController } from './controller/booking.controller';
 import { BookingProvider } from './provider/booking.provider';
@@ -8,13 +8,15 @@ import { CalendarModule } from '@/shared/modules/calender/calender.module';
 import { FlutterwaveModule } from '@/shared/modules/flutterwave/flutterwave.module';
 import { UserModule } from '../user/user.module';
 import { DoxyModule } from '@/shared/modules/doxy/doxy.module';
+import { TokenModule } from '../token/token.module';
 
 @Module({
   imports: [
+    forwardRef(() => TokenModule),
+    forwardRef(() => UserModule),
     ConsultationModule,
     CalendarModule,
     FlutterwaveModule,
-    UserModule,
     DoxyModule,
   ],
   providers: [BookingProvider, BookingService, ErrorHandler],
