e9a97e3ae5932efd4aaa85d8ebd29a66a86217e1
3illBaby <thrillanon@outlook.com>
2025-11-04 22:47:00 +0100

    chore(webhooks.module.ts): import booking module

--- src/modules/webhooks/webhooks.module.ts
+++ src/modules/webhooks/webhooks.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WebhooksService } from './service/webhooks.service';
 import { WebhooksController } from './controller/webhooks.controller';
+import { BookingModule } from '../booking/booking.module';
 
 @Module({
+  imports: [BookingModule],
   providers: [WebhooksService],
   controllers: [WebhooksController],
 })
