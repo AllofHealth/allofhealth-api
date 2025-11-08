7955f3697ff80de6cc6062018a43332a5853802f
3illBaby <thrillanon@outlook.com>
2025-11-08 14:30:07 +0100

    chore(flutterwabe.module.ts): import http module

--- src/shared/modules/flutterwave/flutterwave.module.ts
+++ src/shared/modules/flutterwave/flutterwave.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FlutterwaveService } from './service/flutterwave.service';
 import { FlutterwaveProvider } from './provider/flutterwave.provider';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   providers: [FlutterwaveService, FlutterwaveProvider],
   exports: [FlutterwaveService],
 })
