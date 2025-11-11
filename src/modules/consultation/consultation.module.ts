57663cf1d5f8a2a586af870b4faa9add3a67b49a
3illBaby <thrillanon@outlook.com>
2025-11-11 19:16:49 +0100

    chore(consultation.module.ts): import user and token module

--- src/modules/consultation/consultation.module.ts
+++ src/modules/consultation/consultation.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { ConsultationProvider } from './provider/consultation.provider';
 import { ConsultationService } from './service/consultation.service';
 import { ConsultationController } from './controller/consultation.controller';
 import { ErrorHandler } from '@/shared/error-handler/error.handler';
+import { UserModule } from '../user/user.module';
+import { TokenModule } from '../token/token.module';
 
 @Module({
+  imports: [forwardRef(() => UserModule), forwardRef(() => TokenModule)],
   providers: [ConsultationService, ConsultationProvider, ErrorHandler],
   controllers: [ConsultationController],
   exports: [ConsultationService],
