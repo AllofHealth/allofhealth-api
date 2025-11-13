d2db56240e48c9262a9779138a4c5375024807ec
3illBaby <thrillanon@outlook.com>
2025-11-13 14:23:34 +0100

    chore(resend.data.ts): registered resend constants

--- src/shared/modules/resend/data/resend.data.ts
+++ src/shared/modules/resend/data/resend.data.ts
@@ -15,3 +15,15 @@ export enum RESEND_EMAIL_CONFIG {
   ONBOARDING_SUBJECT = 'Welcome to AllofHealth',
   ONBOARDING_LOGIN_URL = 'https://allofhealth.africa/login',
 }
+
+export enum BOOKING_REQUEST_EMAIL_CONFIG {
+  FROM = 'noreply@allofhealth.africa',
+  SUBJECT = 'Booking Request Received',
+  PAYMENT_URL = 'https://allofhealth.africa/payment',
+}
+
+export enum PAYMENT_CONFIRMED_CONFIG {
+  FROM = 'noreply@allofhealth.africa',
+  SUBJECT = 'Payment Confirmed',
+  PAYMENT_URL = 'https://allofhealth.africa/payment',
+}
