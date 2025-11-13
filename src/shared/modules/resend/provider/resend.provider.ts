0aebc3950f1cd0b3972eb5954a134a0489f69ab2
3illBaby <thrillanon@outlook.com>
2025-11-13 14:24:14 +0100

    chore(resend.provider.ts): impl template handlers

--- src/shared/modules/resend/provider/resend.provider.ts
+++ src/shared/modules/resend/provider/resend.provider.ts
@@ -10,17 +10,26 @@ import {
 import { CreateEmailResponse, Resend } from 'resend';
 import {
   IHandleBooking,
+  IHandleBookingRequest,
   IHandleOnboarding,
   IHandleOtp,
+  IHandlePatientConfirmationEmail,
+  IHandleSendReminderEmail,
   ISendEmail,
 } from '../interface/resend.interface';
 import {
   RESEND_ERROR_MESSAGE as REM,
   RESEND_EMAIL_CONFIG as REC,
   RESEND_SUCCESS_MESSAGE as RSM,
+  BOOKING_REQUEST_EMAIL_CONFIG,
+  PAYMENT_CONFIRMED_CONFIG,
 } from '../data/resend.data';
 import OnboardingEmail from '@/shared/templates/welcome.template';
 import VerificationEmail from '@/shared/templates/otp.template';
+import BookingRequestEmail from '@/shared/templates/booking-request.template';
+import PaymentConfirmationEmail from '@/shared/templates/payment-confirmation.template';
+import PaymentReminderEmail from '@/shared/templates/payment-reminder.template';
+import { IcsService } from '../../ics/service/ics.service';
 
 @Injectable()
 export class ResendProvider {
@@ -28,6 +37,7 @@ export class ResendProvider {
   constructor(
     private readonly handler: ErrorHandler,
     private readonly config: ResendConfig,
+    private readonly icsService: IcsService,
   ) {}
 
   initResend() {
@@ -65,17 +75,188 @@ export class ResendProvider {
     };
   }
 
-  private handleBookingEmailTemplate(ctx: IHandleBooking) {
+  private handleBookingRequestReceivedTemplate(ctx: IHandleBookingRequest) {
+    const {
+      from = BOOKING_REQUEST_EMAIL_CONFIG.FROM,
+      to,
+      subject = BOOKING_REQUEST_EMAIL_CONFIG.SUBJECT,
+      consultationType,
+      date,
+      doctorName,
+      patientName,
+      paymentUrl = BOOKING_REQUEST_EMAIL_CONFIG.PAYMENT_URL,
+      time,
+    } = ctx;
+
+    return {
+      from: from,
+      to: to,
+      subject: subject,
+      react: BookingRequestEmail({
+        consultationType,
+        date,
+        doctorName,
+        patientName,
+        paymentUrl,
+        time,
+      }),
+    };
+  }
+
+  private handlePatientConfirmationTemplate(
+    ctx: IHandlePatientConfirmationEmail,
+  ) {
+    const {
+      from = PAYMENT_CONFIRMED_CONFIG.FROM,
+      subject = PAYMENT_CONFIRMED_CONFIG.SUBJECT,
+      patientName,
+      doctorName,
+      to,
+      startTime,
+      videoRoomUrl,
+      consultationType,
+      calendarUrl,
+      date,
+    } = ctx;
+
+    return {
+      from: from,
+      to: to,
+      subject: subject,
+      react: PaymentConfirmationEmail({
+        calendarUrl: calendarUrl!,
+        consultationType: consultationType!,
+        date: date!,
+        doctorName: doctorName!,
+        patientName: patientName!,
+        time: startTime!,
+        videoRoomUrl: videoRoomUrl!,
+      }),
+    };
+  }
+
+  private handlePaymentReminderTemplate(ctx: IHandleSendReminderEmail) {
+    const {
+      from = PAYMENT_CONFIRMED_CONFIG.FROM,
+      subject = 'Reminder',
+      consultationType,
+      date,
+      paymentUrl = PAYMENT_CONFIRMED_CONFIG.PAYMENT_URL,
+      time,
+      to,
+      patientName,
+      doctorName,
+    } = ctx;
+
+    return {
+      from: from,
+      to: to,
+      subject: subject,
+      react: PaymentReminderEmail({
+        consultationType,
+        date,
+        doctorName,
+        patientName,
+        paymentUrl,
+        time,
+      }),
+    };
+  }
+
+  async sendBookingEmail(ctx: IHandleBooking) {
     const { context } = ctx;
-    switch (context) {
-      case 'PATIENT_CONFIRMATION':
-        break;
-      case 'DOCTOR_NOTIFICATION':
-        break;
-      case 'REMINDER':
-        break;
-      case 'CANCELATION':
-        break;
+    const resend = this.initResend();
+    let response: CreateEmailResponse | null = null;
+    try {
+      switch (context) {
+        case 'BOOKING_CREATED':
+          const bookingConfig = this.handleBookingRequestReceivedTemplate({
+            consultationType: ctx.consultationType!,
+            date: ctx.date!,
+            doctorName: ctx.doctorName!,
+            patientName: ctx.patientName!,
+            time: ctx.time!,
+            to: ctx.to!,
+          });
+
+          const bookingAttachments = this.icsService.generateIcsFile({
+            description: bookingConfig.subject,
+            endTime: new Date(ctx.endTime!),
+            startTime: new Date(ctx.startTime!),
+            title: ctx.consultationType!,
+            url: ctx.paymentUrl!,
+          });
+
+          response = await resend.emails.send({
+            from: bookingConfig.from,
+            subject: bookingConfig.subject,
+            to: bookingConfig.to,
+            react: bookingConfig.react,
+            attachments: [
+              {
+                filename: 'booking_created.ics',
+                content: Buffer.from(bookingAttachments as string),
+              },
+            ],
+          });
+          break;
+        case 'PATIENT_CONFIRMATION':
+          const paymentConfirmationConfig =
+            this.handlePatientConfirmationTemplate({
+              to: ctx.to!,
+              bookingReference: ctx.bookingReference,
+              calendarUrl: ctx.calendarUrl,
+              consultationType: ctx.consultationType,
+              date: ctx.date,
+              doctorName: ctx.doctorName,
+              endTime: ctx.endTime,
+              patientName: ctx.patientName,
+              videoRoomUrl: ctx.videoRoomUrl,
+              startTime: ctx.startTime,
+            });
+
+          const paymentConfirmationAttachments =
+            this.icsService.generateIcsFile({
+              description: paymentConfirmationConfig.subject,
+              endTime: new Date(ctx.endTime!),
+              startTime: new Date(ctx.startTime!),
+              title: ctx.consultationType!,
+              url: ctx.videoRoomUrl!,
+            });
+
+          response = await resend.emails.send({
+            from: paymentConfirmationConfig.from,
+            subject: paymentConfirmationConfig.subject,
+            to: paymentConfirmationConfig.to,
+            react: paymentConfirmationConfig.react,
+            attachments: [
+              {
+                filename: 'consultation.ics',
+                content: Buffer.from(paymentConfirmationAttachments as string),
+              },
+            ],
+          });
+          break;
+        case 'DOCTOR_NOTIFICATION':
+          break;
+        case 'REMINDER':
+          break;
+        case 'CANCELATION':
+          break;
+      }
+
+      if (!response || !response.data || !response.data.id) {
+        this.logger.error('Failed to send email');
+        throw new Error(REM.EMAIL_NOT_SENT);
+      }
+
+      return this.handler.handleReturn({
+        status: HttpStatus.OK,
+        message: RSM.EMAIL_SENT,
+        data: response.data.id,
+      });
+    } catch (e) {
+      return this.handler.handleError(e, REM.ERROR_SENDING_EMAIL);
     }
   }
 
