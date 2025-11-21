c7ab17c2ffb93be5bbb02e3ace7c7f3dbdfebe56
3illBaby <thrillanon@outlook.com>
2025-11-22 00:47:59 +0100

    feat(telemedicine.task.ts): impl cron job to send reminder every 12 hours

--- src/modules/telemedicine/tasks/telemedicine.task.ts
+++ src/modules/telemedicine/tasks/telemedicine.task.ts
@@ -8,8 +8,19 @@ import {
   TELEMED_TASK_ERROR_MESSAGE as TEM,
 } from '../data/telemedicine.data';
 import * as schema from '@/schemas/schema';
-import { and, eq, lt } from 'drizzle-orm';
-import { IPendingBookingData } from '../interface/telemedicine.interface';
+import { and, eq, lt, aliasedTable } from 'drizzle-orm';
+import {
+  IPendingBookingData,
+  IReminderEmailPayload,
+} from '../interface/telemedicine.interface';
+import { BookingService } from '@/modules/booking/service/booking.service';
+import { TelemedicineNotificationsQueue } from '@/shared/queues/telemedicine-notifications/telemedicine-notifications.queue';
+import { TelemedicineError } from '../error/telemedicine.error';
+import {
+  formatDateToReadable,
+  formatTimeReadable,
+} from '@/shared/utils/date.utils';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
 export class TelemedicineTasks {
@@ -17,23 +28,34 @@ export class TelemedicineTasks {
   constructor(
     @Inject(DRIZZLE_PROVIDER) private readonly _db: Database,
     private readonly handler: ErrorHandler,
+    private readonly bookingService: BookingService,
+    private readonly reminderQueue: TelemedicineNotificationsQueue,
   ) {}
 
   private async fetchAllEligibleBookings() {
     try {
+      const patient = aliasedTable(schema.user, 'patient');
+      const doctor = aliasedTable(schema.user, 'doctor');
+
       const pendingBookings = await this._db
         .select({
           id: schema.consultationBookings.id,
-          patientEmail: schema.user.emailAddress,
+          patientId: schema.consultationBookings.patientId,
+          doctorId: schema.consultationBookings.doctorId,
+          externalBookingId: schema.consultationBookings.externalBookingId,
+          patientEmail: patient.emailAddress,
+          patientFullName: patient.fullName,
+          doctorFullName: doctor.fullName,
           consultationType: schema.consultationTypes.name,
           startTime: schema.consultationBookings.startTime,
           endTime: schema.consultationBookings.endTime,
         })
         .from(schema.consultationBookings)
         .leftJoin(
-          schema.user,
-          eq(schema.consultationBookings.patientId, schema.user.id),
+          patient,
+          eq(schema.consultationBookings.patientId, patient.id),
         )
+        .leftJoin(doctor, eq(schema.consultationBookings.doctorId, doctor.id))
         .leftJoin(
           schema.consultationTypes,
           eq(
@@ -66,6 +88,70 @@ export class TelemedicineTasks {
         e,
         e.message || TEM.ERROR_FETCHING_ELIGIBLE_BOOKING,
       );
+    }
+  }
+
+  private async createReminderEmailPayload(ctx: IPendingBookingData[]) {
+    let reminderPayload: IReminderEmailPayload[] = [];
+
+    try {
+      ctx.map(async (data) => {
+        const dataWithPaymentUrl =
+          await this.bookingService.intializeBookingPayment({
+            calcomBookingId: data.externalBookingId!,
+            patientId: data.patientId,
+          });
+
+        if (dataWithPaymentUrl) {
+          const date = data.startTime;
+          const time = data.startTime.getTime();
+          const enrichedData = {
+            patientName: data.patientFullName as string,
+            doctorName: data.doctorFullName as string,
+            consultationType: data.consultationType as string,
+            to: data.patientEmail as string,
+            paymentUrl: dataWithPaymentUrl.data.paymentLink,
+            date: formatDateToReadable(date),
+            time: formatTimeReadable(time),
+          };
+
+          reminderPayload.push(enrichedData);
+        }
+      });
+
+      return reminderPayload;
+    } catch (e) {
+      this.handler.handleError(
+        e,
+        e.message || TEM.ERROR_CREATING_REMINDER_PAYLOAD,
+      );
+    }
+  }
+
+  @Cron(CronExpression.EVERY_12_HOURS)
+  async sendPaymentReminders() {
+    this.logger.log('Execting Reminder Tasks');
+    try {
+      const eligibleBookings = await this.fetchAllEligibleBookings();
+      if (!eligibleBookings || eligibleBookings === undefined) {
+        this.logger.log(`No eligible bookings`);
+        return;
+      }
+      const bookingsWithPaymentUrl =
+        await this.createReminderEmailPayload(eligibleBookings);
+
+      if (!bookingsWithPaymentUrl) {
+        this.logger.log('Could not add payment urls to booking payload');
+        throw new TelemedicineError('Error adding payment url');
+      }
+
+      await this.reminderQueue.handleSendBatchReminderJob(
+        bookingsWithPaymentUrl,
+      );
+
+      this.logger.log(`${eligibleBookings.length} reminders sent`);
+    } catch (e) {
+      this.handler.handleError(e, e.message || TEM.ERROR_RUNNING_REMINDER_TASK);
     }
   }
 }
