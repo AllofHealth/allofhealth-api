f4ac5460a50444c658d9ef4d96b9663083018c00
3illBaby <thrillanon@outlook.com>
2025-11-22 00:46:19 +0100

    chore(telemedicine.data.ts): added data

--- src/modules/telemedicine/data/telemedicine.data.ts
+++ src/modules/telemedicine/data/telemedicine.data.ts
@@ -2,4 +2,6 @@ export const MAX_RETRY_COUNT = 5;
 
 export enum TELEMED_TASK_ERROR_MESSAGE {
   ERROR_FETCHING_ELIGIBLE_BOOKING = 'Error fetching eligible booking',
+  ERROR_CREATING_REMINDER_PAYLOAD = 'Error creating reminder payload',
+  ERROR_RUNNING_REMINDER_TASK = 'Error executing payment reminder tasks',
 }
