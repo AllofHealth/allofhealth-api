cbd12100a578ad7ab130509284ee7dfcc0a74f0b
3illBaby <thrillanon@outlook.com>
2025-11-26 21:33:11 +0100

    test(cal.com.service.spec.ts): init test

--- src/shared/modules/cal.com/service/cal.com.service.spec.ts
+++ src/shared/modules/cal.com/service/cal.com.service.spec.ts
@@ -1,12 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CalComService } from './cal.com.service';
+import { CalComProvider } from '../provider/cal.com.provider';
+import { HttpModule } from '@nestjs/axios';
+import { MyLoggerModule } from '@/modules/my-logger/my-logger.module';
+import { ErrorHandler } from '@/shared/error-handler/error.handler';
+import {
+  CALCOM_API_KEY,
+  CALCOM_API_URL,
+  CALCOM_CLIENT_ID,
+  CALCOM_CLIENT_SECRET,
+  CALCOM_EMBED_URL,
+  CALCOM_REDIRECT_URI,
+  CALCOM_WEBHOOK_SECRET,
+} from '@/shared/data/constants';
+import { CalConfig } from '@/shared/config/cal.com/cal.config';
 
 describe('CalComService', () => {
   let service: CalComService;
+  const CAL_CONFIG = {
+    CALCOM_API_KEY,
+    CALCOM_API_URL,
+    CALCOM_CLIENT_ID,
+    CALCOM_CLIENT_SECRET,
+    CALCOM_EMBED_URL,
+    CALCOM_REDIRECT_URI,
+    CALCOM_WEBHOOK_SECRET,
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CalComService],
+      imports: [HttpModule, MyLoggerModule],
+      providers: [
+        CalComService,
+        CalComProvider,
+        ErrorHandler,
+        { provide: CalConfig, useValue: CAL_CONFIG },
+      ],
     }).compile();
 
     service = module.get<CalComService>(CalComService);
