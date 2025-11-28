cc13b40ff2862ddcb69e74e9a0663812499dcdda
3illBaby <thrillanon@outlook.com>
2025-11-28 17:58:14 +0100

    chore(appointments.module.ts): import error handler

--- src/modules/appointments/appointments.module.ts
+++ src/modules/appointments/appointments.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppointmentsService } from './service/appointments.service';
 import { AppointmentsProvider } from './provider/appointments.provider';
 import { AppointmentsController } from './controller/appointments.controller';
+import { ErrorHandler } from '@/shared/error-handler/error.handler';
 
 @Module({
-  providers: [AppointmentsService, AppointmentsProvider],
+  providers: [AppointmentsService, AppointmentsProvider, ErrorHandler],
   controllers: [AppointmentsController],
 })
 export class AppointmentsModule {}
