9fc57a15ce6ff337a2e9f36ed41270075644182a
3illBaby <thrillanon@outlook.com>
2025-11-28 18:54:37 +0100

    feat(availability.service.ts): exposed func to service layer

--- src/modules/availability/service/availability.service.ts
+++ src/modules/availability/service/availability.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { AvailabilityProvider } from '../provider/availability.provider';
+import { ICreateAvailability } from '../interface/availability.interface';
 
 @Injectable()
-export class AvailabilityService {}
+export class AvailabilityService {
+  constructor(private readonly availabilityProvider: AvailabilityProvider) {}
+
+  async createAvailability(ctx: ICreateAvailability) {
+    return await this.availabilityProvider.createAvailability(ctx);
+  }
+}
