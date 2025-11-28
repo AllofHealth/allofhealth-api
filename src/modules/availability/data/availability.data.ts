6e917b13d3d8d77e0788ebc63338d55b2d5449de
3illBaby <thrillanon@outlook.com>
2025-11-28 20:48:46 +0100

    chore(availability.data.ts): added availability data

--- src/modules/availability/data/availability.data.ts
+++ src/modules/availability/data/availability.data.ts
@@ -2,10 +2,14 @@ export enum AVAILABILITY_ERROR_MESSAGE {
   ERROR_CREATING_AVAILABILITY = 'Error creating availability',
   ERROR_UPDATING_AVAILABILITY = 'Error updating availability',
   ERROR_DELETING_AVAILABILITY = 'Error deleting availability',
+  ERROR_FETCHING_AVAILABILITY = 'Error fetching availability',
+  NO_WEEKDAY_AVAILABILITY_CONFIGURED = 'No weekday availability configured',
+  NO_AVAILABILITY_CONFIGURED = 'No availability configured',
 }
 
 export enum AVAILABILITY_SUCCESS_MESSAGE {
   SUCCESS_CREATING_AVAILABILITY = 'Availability created successfully',
   SUCCESS_UPDATING_AVAILABILITY = 'Availability updated successfully',
   SUCCESS_DELETING_AVAILABILITY = 'Availability deleted successfully',
+  SUCCESS_FETCHING_AVAILABILITY = 'Availability fetched successfully',
 }
