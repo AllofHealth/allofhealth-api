e524be7e8f6651736100ac61cfd98a17af684771
3illBaby <thrillanon@outlook.com>
2025-11-28 19:10:19 +0100

    chore(availability.module.ts): import guard modules

--- src/modules/availability/availability.module.ts
+++ src/modules/availability/availability.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { AvailabilityProvider } from './provider/availability.provider';
 import { ErrorHandler } from '@/shared/error-handler/error.handler';
 import { AvailabilityService } from './service/availability.service';
 import { AvailabilityController } from './controller/availability.controller';
+import { AuthModule } from '../auth/auth.module';
+import { UserModule } from '../user/user.module';
 
 @Module({
+  imports: [forwardRef(() => AuthModule), forwardRef(() => UserModule)],
   providers: [AvailabilityService, AvailabilityProvider, ErrorHandler],
   controllers: [AvailabilityController],
 })
