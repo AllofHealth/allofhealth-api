711ec090851101f3530a7bbbe1ac5651d034365d
3illBaby <thrillanon@outlook.com>
2025-12-09 21:35:24 +0100

    chore(identity.data.ts): updated data

--- src/modules/identity/data/identity.data.ts
+++ src/modules/identity/data/identity.data.ts
@@ -1,7 +1,9 @@
 export enum IDENTITY_ERROR_MESSAGES {
   ERROR_STORING_IDENTIFICATION = 'Error storing identification',
+  ERORR_UPDATING_ID = 'Error updating indentification',
 }
 
 export enum IDENTITY_SUCCESS_MESSAGES {
   SUCCESS_STORING_IDENTIFICATION = 'Identification stored successfully',
+  UPDATE_SUCCESSFUL = 'Iderntification updated successfully',
 }
